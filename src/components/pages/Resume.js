4e56dcb03cea43d5fa6420828c0e809b4e15b8bf
Adam <adam.j.major@gmail.com>
2021-03-18 21:51:52 -0700

    Finish most JSX for resume component

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -15,7 +15,10 @@ class Resume extends React.Component {
         <div className="resume__section resume__section-summary">
           <div className="resume__card">
             <div className="resume__content">
-              <h3 className="heading--two resume__header-summary">Summary</h3>
+              <div className="resume__header">
+                <h3 className="heading--two resume__header-title">Summary</h3>
+                <div>Put thumbnail photo here</div>
+              </div>
               <p className="paragraph paragraph--four">
                 Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.
               </p>
@@ -23,21 +26,48 @@ class Resume extends React.Component {
           </div>
         </div>
 
-
-
-
         <div className="resume__section resume__section-skills">
-          <h3 className="heading--two resume__header-skills">Skills</h3>
-
+          <div className="resume__card">
+            <div className="resume__content">
+              <div className="resume__header">
+                <h3 className="heading--two resume__header-title">Skills</h3>
+                <div>Put thumbnail photo here</div>
+              </div>
+              <p className="paragraph paragraph--four">
+                Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.
+              </p>
+            </div>
+          </div>
         </div>
+
         <div className="resume__section resume__section-work-history">
-          <h3 className="heading--two resume__header-work">Work History</h3>
-
+          <div className="resume__card">
+            <div className="resume__content">
+              <div className="resume__header">
+                <h3 className="heading--two resume__header-title">Work History</h3>
+                <div>Put thumbnail photo here</div>
+              </div>
+              <p className="paragraph paragraph--four">
+                Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.
+              </p>
+            </div>
+          </div>
         </div>
+    
         <div className="resume__section resume__section-education">
-          <h3 className="heading--two resume__header-education">Education</h3>
-
+          <div className="resume__card">
+            <div className="resume__content">
+              <div className="resume__header">
+                <h3 className="heading--two resume__header-title">Education</h3>
+                <div>Put thumbnail photo here</div>
+              </div>
+              <p className="paragraph paragraph--four">
+                Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.
+              </p>
+            </div>
+          </div>
         </div>
+            
       </section>
     );
   }
