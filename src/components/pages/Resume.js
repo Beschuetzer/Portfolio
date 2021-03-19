fb9e4a02e03e68aeef03a798db456249fdc51a45
Adam <adam.j.major@gmail.com>
2021-03-18 19:18:36 -0700

    Add more styling for resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -13,11 +13,19 @@ class Resume extends React.Component {
     return (
       <section className="resume">
         <div className="resume__section resume__section-summary">
-          <h3 className="heading--two resume__header-summary">Summary</h3>
-          <div className="resume__content">
-
+          <div className="resume__card">
+            <div className="resume__content">
+              <h3 className="heading--two resume__header-summary">Summary</h3>
+              <p className="paragraph paragraph--four">
+                Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.
+              </p>
+            </div>
           </div>
         </div>
+
+
+
+
         <div className="resume__section resume__section-skills">
           <h3 className="heading--two resume__header-skills">Skills</h3>
 
