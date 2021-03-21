241246ebfd55014da98c76746560f1388149ad2e
Adam <adam.j.major@gmail.com>
2021-03-21 10:39:13 -0700

    Add hover effects to skills section

--- src/components/pages/resume/SkillsItemSection.js
+++ src/components/pages/resume/SkillsItemSection.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
 class SkillsItemSection extends React.Component {
-  onSectionClick = (e) => {
+  onTitleClick = (e) => {
     e.target?.classList?.toggle('skills__section--open')
   }
 
   render() {
     const { children, title } = this.props;
     return (
-      <div onClick={this.onSectionClick} className="skills__section">
-        <div className="skills__section-title">{title}:</div>
+      <React.Fragment>
+        <div onClick={this.onTitleClick} className="skills__section-title">{title}:</div>
         <div className="skills__section-content">
           {children}
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
