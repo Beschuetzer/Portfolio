d8a529d429e2a9ab4ddcf66dcb0203c8f5cb1e3a
Adam <adam.j.major@gmail.com>
2021-03-22 17:19:42 -0700

    Start work on SkillsPopup

--- src/components/SkillsPopup.js
+++ src/components/SkillsPopup.js
@@ -2,17 +2,43 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class SkillsPopup extends React.Component {
+  relevantProjects = []
+
+  componentDidMount() {
+    //TODO: Filter
+    nodes {
+      createdAt: "2020-06-16T05:21:24Z"
+      description: "This was a module in The Odin Project."
+      homepageUrl: ""
+      name: "git_test"
+      repositoryTopics: {nodes: [{topic: {name: "git"}}]}
+      nodes: [{topic: {name: "git"}}]
+      0: {topic: {name: "git"}}
+      updatedAt: "2021-03-15T18:03:40Z"
+      url: "https://github.com/Beschuetzer/git_test"
+    }
+
+
+    this.relevantProjects = this.props.projects.filter(project => project.)
+  }
 
   renderProjects = (skill) => {
     //TODO: calculate the projects but first need to run calculations after fetching in async action creator
+  }
+
+  renderTableHeaders = () => {
+    //TODO
   }
   
   render() {
     const { position, skill } = this.props;
     return (
       <div className={`skills-popup skills-popup${position}`}>
-        <div className='skills-popup__title'>{skill}</div>
-        {this.renderProjects(skill)}
+        <div className='skills-popup__header'>'{skill}' Projects:</div>
+        <div className='skills-popup__table'>
+          {this.renderTableHeaders()}
+          {this.renderProjects(skill)}
+        </div>
       </div>
     );
   }
@@ -20,7 +46,7 @@ class SkillsPopup extends React.Component {
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    skills: state.projects.skills[ownProps.skill],
+    projects: state.projects,
   }
 }
 
