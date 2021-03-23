87f01576d9d949aef2200cd26cde9c6f773745e8
Adam <adam.j.major@gmail.com>
2021-03-23 10:31:47 -0700

    Change mind and go back to animated text

--- src/components/pages/resume/SkillsItem.js
+++ src/components/pages/resume/SkillsItem.js
@@ -31,7 +31,7 @@ const SkillsItem = ({ title, percent, href, clickSkill }) => {
           </a>
         :
           <div onClick={onParagraphClick} className="skills__title">
-            <p>{title}:</p>
+            {title}:
           </div>
         }
         
