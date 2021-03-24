8a73e30714bf8e4d1d3d3b313de0a80c1bf42655
Adam <adam.j.major@gmail.com>
2021-03-23 21:40:48 -0700

    add three js  example to resume.js

--- src/components/pages/resume/Resume.js
+++ src/components/pages/resume/Resume.js
@@ -1,3 +1,4 @@
+import * as THREE from 'three';
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -13,6 +14,8 @@ import SkillsPopup from '../../SkillsPopup';
 
 class Resume extends React.Component {
   componentDidMount() {
+    this.renderTHREE();
+
     console.log('this.props =', this.props);
     if (this.props.repos?.length > 0) return;
     this.props.getRepositories();
@@ -128,6 +131,28 @@ class Resume extends React.Component {
         <img src="/self-2.png" alt="Adam Major"/>
       </div>
     ,
+  }
+
+  renderTHREE = () => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    camera.position.z = 2;
+
+
+    var animate = function () {
+        requestAnimationFrame(animate);
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        renderer.render(scene, camera);
+    };
+    animate();
   }
 
   renderSections = () => {
