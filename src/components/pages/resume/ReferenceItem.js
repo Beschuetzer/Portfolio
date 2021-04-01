b4eecacf59cc610eb4aec32b041848d1eef462b1
Adam <adam.j.major@gmail.com>
2021-04-01 14:17:58 -0700

    Not scaling right on desktop for references

--- src/components/pages/resume/ReferenceItem.js
+++ src/components/pages/resume/ReferenceItem.js
@@ -2,52 +2,30 @@ import React from 'react';
 
 const ReferenceItem = ({number, name, phone, email, relation, href}) => {
 
-  const relationWords = relation.split(' ');
-  const relationIsMultipleWords = false;
-  // const relationIsMultipleWords = relationWords.length > 1;
-  // const relationMiddleIndex = Math.ceil(relationWords.length / 2);
-
-  // const proposedRelationTopRowOne = relationWords.slice(0, (relationMiddleIndex)).join(' ')
-  // const proposedRelationBottomRowOne = relationWords.slice(relationMiddleIndex).join(' ')
-  // const differenceInCharacterLengthOne = Math.abs(proposedRelationTopRowOne - proposedRelationBottomRowOne).length;
-
-  // const proposedRelationTopRowTwo = relationWords.slice(0, (relationMiddleIndex)).join(' ')
-  // const proposedRelationBottomRowTwo = relationWords.slice(relationMiddleIndex).join(' ')
-  // const differenceInCharacterLengthTwo = Math.abs(proposedRelationTopRowTwo - proposedRelationBottomRowTwo).length;
-
-  // let topRowToUse = proposedRelationTopRowOne;
-  // let bottomRowToUse = proposedRelationBottomRowOne;
-
-  // if (differenceInCharacterLengthOne > differenceInCharacterLengthTwo) {
-  //   topRowToUse = proposedRelationTopRowOne;
-  //   bottomRowToUse = proposedRelationBottomRowOne;
-  // }
-
-  //TODO: test this component with different length relations
-
-  //compare the two different arrangements and use the one with the smaller character length difference
-
   return (
     <React.Fragment>
-      {href ?
-        <a target="_blank" rel='noreferrer' href={href} className='references__name references__name--link'>
-          <span className='references__number'>{number}.</span>
-          {name}:
-        </a>
-      :
-        <div className='references__name'>{name}:</div>
-      }
-      {relationIsMultipleWords ?
-        null
-        // <div className='references__relation-words'>
-        //   <div className='references__relation-word-one'>{topRowToUse}</div>
-        //   <div className='references__relation-word-rest'>{bottomRowToUse}</div>
-        // </div>
-      : 
-        <div className='references__relation'>{relation}</div>
-      }
-      <div className='references__phone'>{phone}</div>
-      <div className='references__email'>{email}</div>
+      <div className='references__name'>
+        <span className='references__number'>{number}.</span>
+        {href ?
+          <a target="_blank" rel='noreferrer' href={href} className='references__name-text references__name-link'>
+            {name}:
+          </a>
+        :
+          <div className='references__name-text'>{name}:</div>
+        }
+      </div>
+      <div className='references__relation'>
+        <span className='references__tag'>Relation:</span>
+        {relation}
+      </div>
+      <div className='references__phone'>
+        <span className='references__tag'>Phone:</span>
+        {phone}
+      </div>
+      <div className='references__email'>
+        <span className='references__tag'>Email:</span>
+        {email}
+      </div>
     </React.Fragment>
   );
 }
