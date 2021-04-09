4e0cb3e251add095c9cd6ddbf0bc87b8d3a51598
Adam <adam.j.major@gmail.com>
2021-04-09 12:28:17 -0700

    Update PageNav to no longer specify colors in it

--- src/components/navbar/PageNav.js
+++ src/components/navbar/PageNav.js
@@ -1,204 +1,191 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-import { setPreviousUrl, setScrollPercent } from "../../actions";
-import { capitalize } from "../../helpers";
+import { setPreviousUrl, setScrollPercent } from '../../actions';
+import { capitalize } from '../../helpers';
 
 class PageNav extends React.Component {
-	static cssClass = "page-nav";
-	static gradientVarName = "--site-nav-linear-gradient";
-	static activeScaleVarName = "--site-nav-active-scale-amount";
-	static activeScaleRange = {
-		desktop: { min: 1.5, max: 1.75 },
-		mobile: { min: 1.05, max: 1.25 },
-		min: 1.5,
-		max: 1.75,
-	};
-	static selectedClass = "page-nav--active";
+  static cssClass = 'page-nav';
+  static gradientVarName = '--site-nav-linear-gradient';
+  static activeScaleVarName = '--site-nav-active-scale-amount';
+  static activeScaleRange = {
+    desktop: {min: 1.5, max: 1.75},
+    mobile: {min: 1.05, max: 1.25},
+    min: 1.5,
+    max: 1.75,
+  };
+  static progressPercent = '0%';
+  static selectedClass = 'page-nav--active';
 
-	componentDidMount() {
-		document.addEventListener("scroll", this.handleScroll);
-		this.updateActiveScaleRange();
-	}
+  componentDidMount() {
+    document.addEventListener('scroll', this.handleScroll);
+    this.updateActiveScaleRange();
+  }
 
-	componentDidUpdate() {
-		this.updateActiveScaleRange();
-	}
+  componentDidUpdate () {
+    this.updateActiveScaleRange();
+  }
 
-	updateActiveScaleRange = () => {
-		if (this.props.isMobile) {
-			PageNav.activeScaleRange.min = PageNav.activeScaleRange.mobile.min;
-			PageNav.activeScaleRange.max = PageNav.activeScaleRange.mobile.max;
-		} else {
-			PageNav.activeScaleRange.min = PageNav.activeScaleRange.desktop.min;
-			PageNav.activeScaleRange.max = PageNav.activeScaleRange.desktop.max;
-		}
-	};
+  updateActiveScaleRange = () => {
+    if (this.props.isMobile) {
+      PageNav.activeScaleRange.min = PageNav.activeScaleRange.mobile.min;
+      PageNav.activeScaleRange.max = PageNav.activeScaleRange.mobile.max;
+    } 
+    else { 
+      PageNav.activeScaleRange.min = PageNav.activeScaleRange.desktop.min;
+      PageNav.activeScaleRange.max = PageNav.activeScaleRange.desktop.max;
+    }
+  }
 
-	getLinearGradient = (percent, docStyle) => {
-		const mainColor = docStyle.getPropertyValue('--color-primary-1');
-		const progressColor = docStyle.getPropertyValue('--color-primary-2')
-		const valueRange = {
-			min: 0.5,
-			max: 1,
-		};
+  getLinearGradient = (percent, docStyle) => {
+		const mainColor = docStyle.getPropertyValue('--color-primary-4')
+		const progressColor = docStyle.getPropertyValue('--color-primary-2').trim();
+    
+		// const valueRange = {
+    //   min: .5,
+    //   max: 1,
+    // }
 
-		const percentToUse =
-			valueRange.min + (valueRange.max - valueRange.min) * (percent / 100);
+    // const percentToUse = valueRange.min + ((valueRange.max - valueRange.min) * (percent / 100))
 
-		return `
+    return `
       linear-gradient(to right, 
-        #{alpha(${progressColor}, ${percentToUse})} 0%, 
-        #{alpha(${progressColor}, ${percentToUse})} ${percent}%,
+        ${progressColor.trim()} 0%, 
+        ${progressColor.trim()} ${percent}%,
         ${mainColor} ${percent}%,
-        ${mainColor} 100%)`;
-	};
+        ${mainColor} 100%)`
+      ;
+  }
 
-	handleScroll = (e) => {
-		const scrollY = window.scrollY;
-		const maxScrollY = document.body.scrollHeight - window.innerHeight;
-		const isEnd = scrollY >= maxScrollY;
-		const boundingRects = [];
-		const sections = document.querySelectorAll("[data-section]");
+  handleScroll = (e) => {
+    const scrollY = window.scrollY;
+    const maxScrollY = document.body.scrollHeight - window.innerHeight;
+    const isEnd = scrollY >= maxScrollY;
+    const boundingRects = [];
+    const sections = document.querySelectorAll('[data-section]');
 
-		let currentSection = null;
-		let indexOfCurrentSection = -1;
-		let percentThroughSection = "";
+    let currentSection = null;
+    let indexOfCurrentSection = -1;
+    let percentThroughSection = '';
 
-		for (let i = 0; i < sections.length; i++) {
-			const section = sections[i];
-			const boundingRect = section.getBoundingClientRect();
-			boundingRects.push(boundingRect);
-			indexOfCurrentSection = i - 1;
+    for (let i = 0; i < sections.length; i++) {
+      const section = sections[i];
+      const boundingRect = section.getBoundingClientRect();
+      boundingRects.push(boundingRect);
+      indexOfCurrentSection = i - 1;
 
-			if (boundingRect.top > 1) {
-				if (i === 0) {
-					currentSection = null;
-				} else {
-					currentSection = sections[indexOfCurrentSection];
-				}
-				const boundingRectToUse =
-					boundingRects[i === 0 ? 0 : indexOfCurrentSection];
-				percentThroughSection =
-					(Math.abs(boundingRectToUse.top) /
-						(Math.abs(boundingRectToUse.top) +
-							Math.abs(boundingRectToUse.bottom))) *
-					100;
-				break;
-			}
-		}
-		this.setGradientPercent(
-			sections,
-			currentSection,
-			percentThroughSection,
-			isEnd,
-			indexOfCurrentSection,
-		);
-	};
+      if (boundingRect.top > 1) {
+        if (i === 0) { 
+          currentSection = null;
+        } else {
+          currentSection = sections[indexOfCurrentSection];
+        }
+        const boundingRectToUse = boundingRects[i === 0 ? 0 : indexOfCurrentSection];
+        percentThroughSection = Math.abs(boundingRectToUse.top) / (Math.abs(boundingRectToUse.top) + Math.abs(boundingRectToUse.bottom))  * 100;
+        break;
+      }
+    }
+    this.setGradientPercent(sections, currentSection, percentThroughSection, isEnd, indexOfCurrentSection);
+  }
 
-	setGradientPercent = (
-		sections,
-		currentSection,
-		percentThroughSection,
-		isEnd,
-		indexOfCurrentSection,
-	) => {
+  setGradientPercent = (sections, currentSection, percentThroughSection, isEnd, indexOfCurrentSection) => {
 		let docStyle = getComputedStyle(document.documentElement);
-		const selectedGradient = this.getLinearGradient(percentThroughSection, docStyle);
-		const isEndGradient = this.getLinearGradient(100, docStyle);
-		const normalGradient = this.getLinearGradient(0, docStyle);
+    const selectedGradient = this.getLinearGradient(percentThroughSection, docStyle);
+    const isEndGradient = this.getLinearGradient(100, docStyle);
+    const normalGradient = this.getLinearGradient(0, docStyle);
+    
+    for (let i = 0; i < sections.length; i++) {
+      let gradientToUse = selectedGradient;
+      let shouldAddActiveClass = true;
+      const section = sections[i];
+      const pageNavSectionName = capitalize(section.dataset.section);
+      const pageNavSectionElement = document.querySelector(`.page-nav__section-${pageNavSectionName}`)
+      const shouldSetEnd = isEnd && i >= indexOfCurrentSection;
 
-		// debugger
 
-		for (let i = 0; i < sections.length; i++) {
-			let gradientToUse = selectedGradient;
-			let shouldAddActiveClass = true;
-			const section = sections[i];
-			const pageNavSectionName = capitalize(section.dataset.section);
-			const pageNavSectionElement = document.querySelector(
-				`.page-nav__section-${pageNavSectionName}`,
-			);
-			const shouldSetEnd = isEnd && i >= indexOfCurrentSection;
+      if (shouldSetEnd) {
+        gradientToUse = isEndGradient;
+      }
+      else if (!currentSection?.className.match(new RegExp(pageNavSectionName, 'ig'))) {
+        gradientToUse = normalGradient;
+        shouldAddActiveClass = false;
+      }
 
-			if (shouldSetEnd) {
-				gradientToUse = isEndGradient;
-			} else if (
-				!currentSection?.className.match(new RegExp(pageNavSectionName, "ig"))
-			) {
-				gradientToUse = normalGradient;
-				shouldAddActiveClass = false;
-			}
+      pageNavSectionElement.style.backgroundImage = gradientToUse;
 
-			pageNavSectionElement.style.backgroundImage = gradientToUse;
+      if (shouldAddActiveClass) {
+        pageNavSectionElement.parentNode?.classList?.add(PageNav.selectedClass);
 
-			if (shouldAddActiveClass) {
-				pageNavSectionElement.parentNode?.classList?.add(PageNav.selectedClass);
+        let amountToScale = PageNav.activeScaleRange.max;
+        if (!shouldSetEnd) {
+          amountToScale = PageNav.activeScaleRange.min + ((PageNav.activeScaleRange.max - PageNav.activeScaleRange.min) * percentThroughSection / 100);
+        }
 
-				let amountToScale = PageNav.activeScaleRange.max;
-				if (!shouldSetEnd) {
-					amountToScale =
-						PageNav.activeScaleRange.min +
-						((PageNav.activeScaleRange.max - PageNav.activeScaleRange.min) *
-							percentThroughSection) /
-							100;
-				}
+        const newValue = `${PageNav.activeScaleVarName}: ${amountToScale}`;
+        document.documentElement.style.cssText += newValue;
 
-				const newValue = `${PageNav.activeScaleVarName}: ${amountToScale}`;
-				document.documentElement.style.cssText += newValue;
-			} else
-				pageNavSectionElement.parentNode?.classList?.remove(
-					PageNav.selectedClass,
-				);
-		}
-	};
+      }
+      else pageNavSectionElement.parentNode?.classList?.remove(PageNav.selectedClass);
 
-	renderSections = () => {
-		const { previousUrl } = this.props;
-		const currentUrl = this.props.match?.url;
+      
+    }
+  }
 
-		if (!previousUrl || previousUrl !== currentUrl)
-			this.props.setPreviousUrl(currentUrl);
+  renderSections = () => {
+    const { previousUrl } = this.props;
+    const currentUrl = this.props.match?.url;
 
-		let sectionNames = [];
-		const sections = document.querySelectorAll("[data-section]");
-		for (let i = 0; i < sections.length; i++) {
-			const section = sections[i];
-			const capitalized = capitalize(section.dataset.section);
-			sectionNames.push(capitalized);
-		}
+    if (!previousUrl || previousUrl !== currentUrl) this.props.setPreviousUrl(currentUrl);
 
-		return sectionNames.map((sectionName, index, array) => {
-			return (
-				<li key={sectionName} className={`${PageNav.cssClass}__section-group`}>
-					<a
-						href={`${this.props.match.url}#${sectionName?.toLowerCase()}`}
-						className={`${PageNav.cssClass}__section ${PageNav.cssClass}__section-${sectionName}`}>
-						{sectionName}
-					</a>
-				</li>
-			);
-		});
-	};
+    let sectionNames = [];
+    const sections = document.querySelectorAll('[data-section]');
+    for (let i = 0; i < sections.length; i++) {
+      const section = sections[i];
+      const capitalized = capitalize(section.dataset.section);
+      sectionNames.push(capitalized)
+    }
+    
+    return sectionNames.map((sectionName, index, array) => {
+      return (
+        <li 
+          key={sectionName} 
+          className={`${PageNav.cssClass}__section-group`}
+        >
+          <a 
+            href={`${this.props.match.url}#${sectionName?.toLowerCase()}`} 
+            className={`${PageNav.cssClass}__section ${PageNav.cssClass}__section-${sectionName}`}
+          >
+            {sectionName}
+          </a>
+        </li>
+      );
+    });
+  }
 
-	render() {
-		return ReactDOM.createPortal(
-			//The idea behind this component is to have a nav element that has quick links  to the sections of each page
-			<React.Fragment>{this.renderSections()}</React.Fragment>,
-			document.querySelector(".page-nav"),
-		);
-	}
+  render() {
+    return (
+      ReactDOM.createPortal(
+        //The idea behind this component is to have a nav element that has quick links  to the sections of each page
+        <React.Fragment>
+          {this.renderSections()}
+        </React.Fragment>
+      ,
+        document.querySelector('.page-nav')
+      )
+    );
+  }
 }
 
 const mapStateToProps = (state, ownProps) => {
-	return {
-		previousUrl: state.general.previousUrl,
-		isMobile: state.general.isMobile,
-	};
-};
+  return { 
+    previousUrl: state.general.previousUrl,
+    isMobile: state.general.isMobile,
+  }
+}
 
 export default connect(mapStateToProps, {
-	setPreviousUrl,
-	setScrollPercent,
+  setPreviousUrl,
+  setScrollPercent,
 })(PageNav);
