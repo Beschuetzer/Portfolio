bc8163eed8ec357551c0f3d4b1be41479a19eb39
Adam <adam.j.major@gmail.com>
2021-04-12 22:05:35 -0700

    Finish diamond

--- frontend/src/components/pages/examples/Bridge/BridgeHero.js
+++ frontend/src/components/pages/examples/Bridge/BridgeHero.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const BridgeHero = () => {
+  //my idea here is to have a centered diamond background that has has the section or A # Maj Bridge words around the four corners.  Inside the diamond, there are other suits and a background video?
   return (
-    <div>
-      BridgeHero
-    </div>
+    <article className="bridge__hero">
+      <div className="bridge__diamond bridge__diamond-left"></div>
+      <div className="bridge__diamond bridge__diamond-right"></div>
+      <h2 className="bridge__hero-heading heading--two">The Beginnings</h2>
+      <h2 className="bridge__hero-heading heading--two">of a Developer</h2>
+    </article>
   );
 }
 
