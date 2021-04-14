933eb1d675572df39a579786d73c2b63bf102e33
Adam <adam.j.major@gmail.com>
2021-04-14 10:02:44 -0700

    Add concentric squares

--- frontend/src/components/pages/examples/Bridge/BridgeHero.js
+++ frontend/src/components/pages/examples/Bridge/BridgeHero.js
@@ -4,15 +4,25 @@ const BridgeHero = () => {
   //my idea here is to have a centered diamond background that has has the section or A # Maj Bridge words around the four corners.  Inside the diamond, there are other suits and a background video?
   return (
     <article className="bridge__hero">
-      <div className="diamond">
-        <div className="bridge__hero-text">
-          <h2 className="bridge__hero-heading heading--one">A</h2>
-          <h2 className="bridge__hero-heading heading--one">#</h2>
-          <h2 className="bridge__hero-heading heading--two">Maj</h2>
-          <h2 className="bridge__hero-heading heading--two">Bridge</h2>
+      {/* <div className="diamond"> */}
+      <div className='square-container'>
+        <div className="square square-outer"> 
+    
+    
+          <div className="bridge__hero-text">
+            <h2 className="bridge__hero-heading heading--one">A</h2>
+            <h2 className="bridge__hero-heading heading--one">#</h2>
+            <h2 className="bridge__hero-heading heading--two">Maj</h2>
+            <h2 className="bridge__hero-heading heading--two">Bridge</h2>
+          </div>
+
         </div>
+        <div className="square square-inner1 square-inner-odd"> </div>
+        <div className="square square-inner2 square-inner-even"> </div>
+        <div className="square square-inner3 square-inner-odd"> </div>
+        <div className="square square-inner4 square-inner-even"> </div>
       </div>
-      <div className="line"></div>
+      {/* <div className="line"></div> */}
     </article>
   );
 }
