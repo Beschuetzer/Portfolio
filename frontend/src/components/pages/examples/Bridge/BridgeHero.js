18a82a6c9fe4a7ba7dddacb93f31aceed94ad940
Adam <adam.j.major@gmail.com>
2021-04-14 12:52:54 -0700

    Add labels to bridge page

--- frontend/src/components/pages/examples/Bridge/BridgeHero.js
+++ frontend/src/components/pages/examples/Bridge/BridgeHero.js
@@ -1,22 +1,19 @@
-import React from 'react';
-import SquareFractal from './SquareFractal';
+import React from "react";
+import DiamondFractal from "./DiamondFractal";
 
 const BridgeHero = () => {
-  //my idea here is to have a centered diamond background that has has the section or A # Maj Bridge words around the four corners.  Inside the diamond, there are other suits and a background video?
-  return (
-    <article className="bridge__hero">
-      {/* <div className="diamond"> */}
-      {/* <div className="bridge__hero-text">
-        <h2 className="bridge__hero-heading heading--one">A</h2>
-        <h2 className="bridge__hero-heading heading--one">#</h2>
-        <h2 className="bridge__hero-heading heading--two">Maj</h2>
-        <h2 className="bridge__hero-heading heading--two">Bridge</h2>
-      </div> */}
-      <SquareFractal
-        topLeftWord="Making"
-      />
-    </article>
-  );
-}
+	//my idea here is to have a centered diamond background that has has the section or A # Maj Bridge words around the four corners.  Inside the diamond, there are other suits and a background video?
+	return (
+		<article className="bridge__hero">
+			{/* <div className="diamond"> */}
+      <span className="bridge__hero-heading heading--one">Making</span>
+      <span className="bridge__hero-heading heading--one">A#</span>
+      <span className="bridge__hero-heading heading--one"></span>
+      <span className="bridge__hero-heading heading--one">&nbsp;Maj</span>
+      <span className="bridge__hero-heading heading--one">Bridge</span>
+			<DiamondFractal topLeftWord="Making" />
+		</article>
+	);
+};
 
 export default BridgeHero;
