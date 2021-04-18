2aa3c19f8dd3444b540a49cee70d4ee115168d78
Adam <adam.j.major@gmail.com>
2021-04-17 21:06:13 -0700

    add hero__more

--- frontend/src/components/pages/examples/Bridge/BridgeHero.js
+++ frontend/src/components/pages/examples/Bridge/BridgeHero.js
@@ -6,18 +6,26 @@ import DiamondFractal from "./DiamondFractal";
 
 const BridgeHero = () => {
 	//my idea here is to have a centered diamond background that has has the section or A # Maj Bridge words around the four corners.  Inside the diamond, there are other suits and a background video?
+  const handleClick = (e) => {
+    
+  }
+
 	return (
     <div className="hero">
       {/* <div className="diamond"> */}
-      <span className="hero__heading heading--one">Making</span>
+      {/* <span className="hero__heading heading--one">Making</span>
       <span className="hero__heading heading--one">A#</span>
       <span className="hero__heading heading--one">&nbsp;Maj</span>
-      <span className="hero__heading heading--one">Bridge</span>
+      <span className="hero__heading heading--one">Bridge</span> */}
       {/* <DiamondFractal topLeftWord="Making" /> */}
 
 
 
-    
+      <div onClick={handleClick} className="hero__more">
+        <svg>
+          <use xlinkHref="/sprite.svg#icon-help"></use>
+        </svg>
+      </div>
 
       <div className="hero__background">Background layer</div>
       <BackgroundVideo
