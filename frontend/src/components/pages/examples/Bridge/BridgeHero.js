e59e5e145d50545120bd0b030233a074e8cb2ef7
Adam <adam.j.major@gmail.com>
2021-04-17 21:40:48 -0700

    finish hero__more-checkbox

--- frontend/src/components/pages/examples/Bridge/BridgeHero.js
+++ frontend/src/components/pages/examples/Bridge/BridgeHero.js
@@ -6,8 +6,8 @@ import DiamondFractal from "./DiamondFractal";
 
 const BridgeHero = () => {
 	//my idea here is to have a centered diamond background that has has the section or A # Maj Bridge words around the four corners.  Inside the diamond, there are other suits and a background video?
-  const handleClick = (e) => {
-    
+  const handleMoreClick = (e) => {
+    // e.target.className = 'hero__more hero__more--open';
   }
 
 	return (
@@ -21,10 +21,13 @@ const BridgeHero = () => {
 
 
 
-      <div onClick={handleClick} className="hero__more">
-        <svg>
-          <use xlinkHref="/sprite.svg#icon-help"></use>
-        </svg>
+      <input id='hero__more-checkbox' type='checkbox'></input>
+      <div onClick={handleMoreClick} className="hero__more">
+        <label for='hero__more-checkbox'>
+          <svg>
+            <use xlinkHref="/sprite.svg#icon-help"></use>
+          </svg>
+        </label>
       </div>
 
       <div className="hero__background">Background layer</div>
