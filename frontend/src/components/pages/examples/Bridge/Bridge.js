21c5c9cb599c3015e06c8741404f192df7080c55
Adam <adam.j.major@gmail.com>
2021-04-22 19:46:57 -0700

    Add summary section

--- frontend/src/components/pages/examples/Bridge/Bridge.js
+++ frontend/src/components/pages/examples/Bridge/Bridge.js
@@ -2,13 +2,86 @@ import React from "react";
 import { useEffect } from "react";
 
 import BridgeHero from './BridgeHero';
+import BridgeCard from './BridgeCard';
+import SectionContainer from './../../../SectionContainer';
 import Card from '../../../Card';
 import ArrowButton from '../../../ArrowButton';
-import SectionContainer from './../../../SectionContainer';
 
 
 
 const Bridge = () => {
+	const sectionContents = [
+			<SectionContainer
+				name='summary'
+				pageName='bridge'
+			>
+				<p>
+					The outbreak of the COVID-19 Pandemic provided me with a lot of time to reflect on my career choices and where I want to be in 10 years time.  
+				</p>
+				<p>
+					After much careful reflection, I realized that I would not be able to live a self-realized life, if I did not at least try to become a professional coder. 
+				</p>
+			</SectionContainer>
+		,
+			<SectionContainer
+				name='motivation'
+				pageName='bridge'
+			>
+				<BridgeCard
+					titleSize='two'
+					titleContent='Why Bridge?'
+				>
+					<ul className='bridge__reasons'>
+							<li className='bridge__reasons-item'>
+								There was no free online bridge resource in May of 2020. 
+							</li>
+							<li className='bridge__reasons-item'>
+								A good friend from MN mentioned how much he missed playing Bridge now that he couldn’t play Bridge in person due to the COVID 19 pandemic. 
+							</li>
+							<li className='bridge__reasons-item'>
+								My current position at Ricoh was not panning out as I had expected. 
+							</li>
+							<li className='bridge__reasons-item'>
+								I have always been enamored with the idea of becoming a computer programmer (my dad was a programmer for a company called Deluxe in MN.  However, I saw how much my dad’s new position as a project manager caused him a lot of stress, which eventually to a heart attack.  I naively attributed his heart attack to coding in my mind and decided to pursue other things, even though I had a thing for programming.), and I finally realized that this may be my last chance to become a programmer.  I saw A#Maj as a test of whether or not I had what it takes to become a programmer.
+							</li>
+						</ul>
+
+					{/* <Card
+						cardName='ace-of-clubs'
+						title='Why?'
+					>
+					</Card>
+					<Card
+						cardName='ace-of-diamonds'
+						title='How?'
+					>
+					</Card>
+					<Card
+						cardName='ace-of-hearts'
+						title='Challenges'
+					>
+					</Card>
+					<Card
+						cardName='ace-of-spades'
+						title='Lessons Learned'
+					>
+					</Card> */}
+				</BridgeCard>
+				{/* <ArrowButton
+					direction='left'
+					fillNumber='1'
+					hoverFillNumber='3'
+				/>
+				<ArrowButton
+					direction='right'
+					fillNumber='1'
+					hoverFillNumber='4'
+				/> */}
+			</SectionContainer>
+	]
+
+
+
 	useEffect(() => {
 		const newLinearGradient = `
       linear-gradient(to right, var(--color-primary-1), var(--color-primary-1));
@@ -18,6 +91,10 @@ const Bridge = () => {
 			newLinearGradient,
 		);
 	}, []);
+
+	const renderSections = () => {
+		return sectionContents.map(item => item);
+	}
 
 	return (
 		<React.Fragment>
@@ -30,47 +107,7 @@ const Bridge = () => {
 					Contract bridge, or simply bridge, is a trick-taking card game using a standard 52-card deck. In its basic format, it is played by four players in two competing partnerships, with partners sitting opposite each other around a table.
 				</p> */}
 			{/* <div className="transition">Transition</div> */}
-			<SectionContainer
-				pageName='bridge'
-				name='why'
-			>
-				<div className="bridge__inner">
-					<h2 className='heading--two bridge__section-title'>Why Bridge?</h2>
-					<div className='bridge__section-content'>
-						{/* <Card
-							cardName='ace-of-clubs'
-							title='Why?'
-						>
-						</Card>
-						<Card
-							cardName='ace-of-diamonds'
-							title='How?'
-						>
-						</Card>
-						<Card
-							cardName='ace-of-hearts'
-							title='Challenges'
-						>
-						</Card>
-						<Card
-							cardName='ace-of-spades'
-							title='Lessons Learned'
-						>
-						</Card> */}
-					</div>
-				</div>
-
-				<ArrowButton
-					direction='left'
-					fillNumber='1'
-					hoverFillNumber='3'
-				/>
-				<ArrowButton
-					direction='right'
-					fillNumber='1'
-					hoverFillNumber='4'
-				/>
-			</SectionContainer>
+			{renderSections()}
 		</div>
 
 	</React.Fragment>
---
+++ frontend/src/components/pages/examples/Bridge/BridgeCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const BridgeCard = ({children, titleSize = 'two', titleContent}) => {
+  return (
+    <div className="bridge__inner">
+      <h2 className={`heading--${titleSize} bridge__section-title`}>{titleContent}</h2>
+      <div className='bridge__section-content'>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default BridgeCard;
