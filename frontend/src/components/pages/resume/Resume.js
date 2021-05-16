316e9f373fcfeb970ce6cbdb57a6e2d047a8409b
Adam <adam.j.major@gmail.com>
2021-05-15 21:04:19 -0700

    Add FirEasiest

--- frontend/src/components/pages/resume/Resume.js
+++ frontend/src/components/pages/resume/Resume.js
@@ -13,9 +13,6 @@ import EducationItem from "../resume/EducationItem";
 import { getRepositories, setSectionsToSkipAnimation } from "../../../actions";
 import SkillsPopup from "./Skills/SkillsPopup/SkillsPopup";
 import WorkHistoryItem from "./WorkHistory/WorkHistoryItem";
-import Fire from '../../../libs/Fire';
-import FireShader from '../../../libs/FireShader';
-
 class Resume extends React.Component {
 	static popupUrl = "/resume#skillsPopup";
 	static skillsItemSectionLabels = [
@@ -529,72 +526,72 @@ class Resume extends React.Component {
 	}
 
 	renderTHREE = () => {
-		var camera, scene, renderer;
-		var mesh;
-		var clock, controller, fire;
+		// var camera, scene, renderer;
+		// var mesh;
+		// var clock, controller, fire;
 
-		init();
+		// init();
 
-		function init() {
+		// function init() {
 
-				camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.001, 1000);
-				camera.position.z = 2;
+		// 		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.001, 1000);
+		// 		camera.position.z = 2;
 
-				scene = new THREE.Scene();
+		// 		scene = new THREE.Scene();
 
-				renderer = new THREE.WebGLRenderer();
-				renderer.setPixelRatio( window.devicePixelRatio );
-				renderer.setSize( window.innerWidth, window.innerHeight );
-				document.body.appendChild( renderer.domElement );
+		// 		renderer = new THREE.WebGLRenderer();
+		// 		renderer.setPixelRatio( window.devicePixelRatio );
+		// 		renderer.setSize( window.innerWidth, window.innerHeight );
+		// 		document.body.appendChild( renderer.domElement );
 
-				var loader = new THREE.TextureLoader();
-				loader.crossOrigin = '';
+		// 		var loader = new THREE.TextureLoader();
+		// 		loader.crossOrigin = '';
 
-				var fireTex = loader.load("https://s3-us-west-2.amazonaws.com/s.cdpn.io/212131/Fire.png");
+		// 		var fireTex = loader.load("https://s3-us-west-2.amazonaws.com/s.cdpn.io/212131/Fire.png");
 
-				var wireframeMat = new THREE.MeshBasicMaterial({
-						color : new THREE.Color(0xffffff),
-						wireframe : true
-				});
+		// 		var wireframeMat = new THREE.MeshBasicMaterial({
+		// 				color : new THREE.Color(0xffffff),
+		// 				wireframe : true
+		// 		});
 
-				fire = new THREE.Fire(fireTex);
+		// 		fire = new THREE.Fire(fireTex);
 
-				var wireframe = new THREE.Mesh(fire.geometry, wireframeMat.clone());
-				fire.add(wireframe);
-				wireframe.visible = true;
-				wireframe.visible = false;
+		// 		var wireframe = new THREE.Mesh(fire.geometry, wireframeMat.clone());
+		// 		fire.add(wireframe);
+		// 		wireframe.visible = true;
+		// 		wireframe.visible = false;
 				
-				console.log(fire);
-				fire.position.set(0, 0, 0);
-				fire.position.set(0, 0.25, 1.3);
-				scene.add(fire);
+		// 		console.log(fire);
+		// 		fire.position.set(0, 0, 0);
+		// 		fire.position.set(0, 0.25, 1.3);
+		// 		scene.add(fire);
 
-				clock = new THREE.Clock();
+		// 		clock = new THREE.Clock();
 
-				window.addEventListener( 'resize', onWindowResize, false );
+		// 		window.addEventListener( 'resize', onWindowResize, false );
 
-		}
+		// }
 
-		function onWindowResize() {
+		// function onWindowResize() {
 
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
+		// 		camera.aspect = window.innerWidth / window.innerHeight;
+		// 		camera.updateProjectionMatrix();
 
-				renderer.setSize( window.innerWidth, window.innerHeight );
+		// 		renderer.setSize( window.innerWidth, window.innerHeight );
 
-		}
+		// }
 
-		(function loop() {
-				requestAnimationFrame(loop);
+		// (function loop() {
+		// 		requestAnimationFrame(loop);
 
-				var delta = clock.getDelta();
+		// 		var delta = clock.getDelta();
 
-				//var t = clock.elapsedTime * controller.speed;
-				var t = clock.elapsedTime;
-				fire.update(t);
+		// 		//var t = clock.elapsedTime * controller.speed;
+		// 		var t = clock.elapsedTime;
+		// 		fire.update(t);
 				
-				renderer.render(scene, camera);
-		})();
+		// 		renderer.render(scene, camera);
+		// })();
 	};
 
 	renderSections = () => {
