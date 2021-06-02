6e72a4ac33ba2c2aae24991840e871eed952d7b2
Adam <adam.j.major@gmail.com>
2021-06-02 10:10:00 -0700

    Add arrow buttons to carousel

--- frontend/src/components/Carousel.js
+++ frontend/src/components/Carousel.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import ArrowButton from './ArrowButton';
 
 const Carousel = ({images, alts}) => {
 
-  const handleClick = (e) => {
+  const handleImageClick = (e) => {
     console.log('e =', e);
     const image = e.currentTarget;
     if (!image) return;
     e.preventDefault();
     image.classList.toggle('full-screen');
+  }
+
+  const handleArrowClick = (e) => {
+
   }
 
   const renderImages = () => {
@@ -19,7 +24,7 @@ const Carousel = ({images, alts}) => {
             className="carousel__image" 
             alt={alts[index]}
             key={index}
-            onClick={handleClick}
+            onClick={handleImageClick}
           />
         // </article>
       )
@@ -27,9 +32,21 @@ const Carousel = ({images, alts}) => {
   }
 
   return (
-    <article className="carousel">
-      {renderImages()}
-    </article>
+    <React.Fragment>
+      <article className="carousel">
+        {renderImages()}
+      </article>
+      <div onClick={handleArrowClick} className={`hidden  carousel__arrow-button carousel__arrow-button--left`}>
+        <svg> 
+          <use xlinkHref="/sprite.svg#icon-arrow-with-circle-down"></use>
+        </svg>
+      </div>
+      <div onClick={handleArrowClick} className={` carousel__arrow-button carousel__arrow-button--right `}>
+        <svg> 
+          <use xlinkHref="/sprite.svg#icon-arrow-with-circle-down"></use>
+        </svg>
+      </div>
+    </React.Fragment>
   );
 }
 
