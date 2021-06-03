292255a9133bdeac48818ee87b8077f4cb1fcc71
Adam <adam.j.major@gmail.com>
2021-06-03 10:15:31 -0700

    Add CSharpLayout

---
+++ frontend/src/components/CSharpLayout.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Section from "./Section";
+import SectionContainer from "./SectionContainer";
+import SourceCodeLink from "./SourceCodeLink";
+
+const CSharpLayout = ({
+	sections,
+	pageNameSecondary: pageName,
+	sourceCodeLink,
+	sourceCodeMsg = "Code",
+	children,
+}) => {
+  const CSS_NAME = "csharp";
+
+	function renderSections() {
+		return sections.map((section, index) => {
+			return (
+				<Section key={index} name={section.name} pageName={section.pageName}>
+					{section.children.map((child, index) => {
+						return <React.Fragment key={index}>{child}</React.Fragment>;
+					})}
+				</Section>
+			);
+		});
+	}
+
+	return (
+		<div className={`${CSS_NAME} ${pageName}`}>
+			<SectionContainer name="Images" pageName={CSS_NAME}>
+				<SourceCodeLink href={sourceCodeLink} msg={sourceCodeMsg} />
+				<div className={`${CSS_NAME}__title`}>{pageName.toUpperCase()}</div>
+				{children}
+			</SectionContainer>
+			{renderSections()}
+		</div>
+	);
+};
+
+export default CSharpLayout;
