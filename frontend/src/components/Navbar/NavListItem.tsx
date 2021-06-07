b79a35b6fe8a7e0d409906bb3c7320dd1b46c1e8
Adam <adam.j.major@gmail.com>
2021-06-06 20:00:24 -0700

    Convert NavListItem to .tsx

---
+++ frontend/src/components/Navbar/NavListItem.tsx
@@ -0,0 +1,97 @@
+import React, { ReactChildren } from "react";
+import { Link } from 'react-router-dom';
+
+interface NavItemProps {
+	isLink?: boolean
+  to: string;
+  label: string;
+  children: ReactChildren;
+  className: string;
+  triangle: HTMLElement;
+  imageAlt?: string;
+  imageSource?: string;
+  onMouseEnter: (e: any) => void;
+  onClick: (e: any) => void;
+}
+
+const NavItem: React.FC<NavItemProps> = ({
+	isLink = true,
+  onMouseEnter,
+  onClick,
+  to,
+  label,
+  children,
+  className,
+  triangle,
+  imageAlt = "A picture",
+  imageSource = "",
+}) => {
+	
+  const defaults = {
+		liClassName: "navbar__item",
+		linkClassName: "navbar__link",
+		imageClassName: "navbar__link-image",
+	};
+
+  const classNamesToUse = className ? className : defaults.liClassName;
+  const getContent = () => {
+    let content = (
+      <React.Fragment>
+        <div className="navbar__dropdown-group">
+          {label}
+          {triangle}
+        </div>
+        {children}
+      </React.Fragment>
+    );
+
+    if (!triangle) {
+      content = (
+        <React.Fragment>
+          {label}
+          {children}
+        </React.Fragment>
+      );
+    }
+
+    return content;
+  };
+
+  return (
+    <li
+      onMouseEnter={onMouseEnter}
+      onClick={onClick}
+      className={`${classNamesToUse}  ${
+        isLink && imageSource ? "overflow-hidden" : ""
+      }`}>
+      {isLink && imageSource ? (
+        <img
+          className={defaults.imageClassName}
+          src={imageSource}
+          alt={imageAlt}
+        />
+      ) : null}
+      {isLink ? (
+        <Link className={defaults.linkClassName} to={to}>
+          {getContent()}
+        </Link>
+      ) : (
+        <div
+          className={`${defaults.linkClassName} ${
+            !isLink && imageSource ? "overflow-hidden" : ""
+          }`}>
+          {!isLink && imageSource ? (
+            <img
+              className={defaults.imageClassName}
+              src={imageSource}
+              alt={imageAlt}
+            />
+          ) : null}
+          {getContent()}
+        </div>
+      )}
+    </li>
+  );
+}
+
+export default NavItem;
