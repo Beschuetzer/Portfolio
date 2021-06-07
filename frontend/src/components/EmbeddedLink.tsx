f6d6bda89cf3e21014103c15f9ad30c167c055b9
Adam <adam.j.major@gmail.com>
2021-06-07 12:59:21 -0700

    Convert EmbeddedLink to .tsx

---
+++ frontend/src/components/EmbeddedLink.tsx
@@ -0,0 +1,42 @@
+import React, { ReactChildren } from "react";
+import { Link } from "react-router-dom";
+
+interface EmbeddedLinkProps {
+	href: string,
+	className?: string,
+	isLocal?: boolean,
+	children: ReactChildren,
+}
+
+const EmbeddedLink: React.FC<EmbeddedLinkProps> = ({
+	href,
+	className = "bridge__link",
+	isLocal = false,
+	children,
+}) => {
+	const renderContent = () => {
+		if (isLocal) {
+			return (
+				<Link to={href} className={className}>
+					{children}
+				</Link>
+			);
+		}
+
+		return (
+			<a target="_blank" rel="noreferrer" href={href} className={className}>
+				{children}
+			</a>
+		);
+	};
+
+	return (
+		<React.Fragment>
+			&nbsp;
+			{renderContent()}
+			&nbsp;
+		</React.Fragment>
+	);
+};
+
+export default EmbeddedLink;
