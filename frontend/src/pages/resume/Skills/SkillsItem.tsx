9849df371f75554b5817c425368698b0e2e889cd
Adam <adam.j.major@gmail.com>
2021-06-08 13:11:25 -0700

    Convert SkillsItem to .tsx

---
+++ frontend/src/pages/resume/Skills/SkillsItem.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { useRef, useState, useEffect } from "react";
+import { connect } from "react-redux";
+import { clickSkill } from "../../../actions";
+import SkillsItemSectionLabels from "./SkillsItemSectionLabels";
+
+interface SkillsItemProps {
+  title: string,
+  percent: string,
+  href: string,
+  hours: string,
+  labels: string,
+  clickSkill: (value: HTMLElement) => void,
+}
+
+const SkillsItem: React.FC<SkillsItemProps> = ({ title, percent, href, clickSkill, hours, labels }) => {
+	const skillsPopupDiv = document.querySelector("#skillsPopup");
+	const percentDiv = useRef<any>(null);
+	const [isDivSet, setIsDivSet] = useState(false);
+
+	const onParagraphClick = (e: MouseEvent) => {
+		if (
+			((e.target as HTMLElement)?.parentNode?.parentNode as any)?.previousElementSibling?.textContent.search(
+				/human/i,
+			) !== -1
+		)
+			return;
+
+		skillsPopupDiv?.classList?.toggle("skills-popup--active");
+		clickSkill(e.target as HTMLElement);
+	};
+
+	useEffect(() => {
+		percentDiv.current.style.width = `${percent}%`;
+		setIsDivSet(true);
+	}, [percentDiv, percent, isDivSet]);
+
+	return (
+		<React.Fragment>
+			<li className="skills__item">
+				<svg className="skills__section-svg">
+					<use xlinkHref="/sprite.svg#icon-circle"></use>
+				</svg>
+				{href ? (
+					<a
+						target="_blank"
+						rel="noreferrer"
+						className="skills__title"
+						href={href}>
+						{title}:
+					</a>
+				) : (
+					<div onClick={(e: any) => onParagraphClick(e)} className="skills__title">
+						{title}:
+					</div>
+				)}
+			</li>
+			<div className="skills__percent-outer">
+				<SkillsItemSectionLabels labels={labels} />
+				<div ref={percentDiv} className="skills__percent-inner">
+					<div className="skills__hours">
+						{hours ? `~ ${hours} hours` : null}
+					</div>
+				</div>
+				<div className="skills__percent-outer-left"></div>
+				<div className="skills__percent-outer-center"></div>
+				<div className="skills__percent-outer-right"></div>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default connect(null, {
+	clickSkill,
+})(SkillsItem);
