f6415a81addf78e3f2cfe27966910f4c85477b1b
Adam <adam.j.major@gmail.com>
2021-06-08 16:00:53 -0700

    Convert SkillsItemSEction to .tsx

---
+++ frontend/src/pages/resume/Skills/SkillsItemSection.tsx
@@ -0,0 +1,45 @@
+import { connect, RootStateOrAny } from 'react-redux';
+import { SKILLS_CLASSNAME, toggleItem } from './utils';
+
+interface SkillsItemSectionProps {
+  title: string,
+  children: any,
+  sectionsToSkipAnimation: any[],
+}
+
+const SkillsItemSection: React.FC<SkillsItemSectionProps> = ({
+  title,
+  children,
+  sectionsToSkipAnimation
+}) => {
+
+  const onTitleClick = (e: MouseEvent) => {
+    e.stopPropagation();
+    toggleItem(e, sectionsToSkipAnimation);
+  }
+
+  return (
+    <div className={`${SKILLS_CLASSNAME}__section`}>
+      <div onClick={(e: any) => onTitleClick(e)} className={`${SKILLS_CLASSNAME}__section-title ${SKILLS_CLASSNAME}__title--animating`}>
+        {title}
+        <svg className={`${SKILLS_CLASSNAME}__section-title-svg`}>
+            <use xlinkHref="/sprite.svg#icon-angle-right"></use>
+          </svg>
+      </div>
+      <div className={`${SKILLS_CLASSNAME}__section-content`}>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+const mapStateToProps = (state: RootStateOrAny) => {
+  return {
+    sectionsToSkipAnimation: state.resume.sectionsToSkipAnimation,
+  }
+}
+
+export default connect(mapStateToProps,
+{
+
+})(SkillsItemSection);
--- frontend/src/pages/resume/Skills/utils.ts
+++ frontend/src/pages/resume/Skills/utils.ts
@@ -3,3 +3,31 @@ export interface Repository {
 }
 
 export const SKILLS_CLASSNAME = "skills";
+export const SKILLS_SECTION_CLASSNAME = `${SKILLS_CLASSNAME}__section`;
+export const SKILLS_SECTION_OPEN_CLASSNAME = `${SKILLS_CLASSNAME}__section--open`;
+export const TIME_OUT_DIFFERENTIAL = 50;
+
+export const toggleItem = (
+  e: MouseEvent,
+  sectionsToSkipAnimation: any[],
+) => {
+  const clickedSection = e.target as HTMLElement;
+  if (!clickedSection.nextSibling) return;
+  if (clickedSection.parentNode?.querySelector(`.${SKILLS_SECTION_CLASSNAME}`)) return;
+
+  const items = (clickedSection.nextSibling as HTMLElement)?.querySelectorAll(`.${SKILLS_CLASSNAME}__percent-outer`);
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i];
+    setTimeout(() => {
+      item.classList.toggle('scale-1');
+
+      if (sectionsToSkipAnimation.indexOf(clickedSection.textContent) === -1) {
+        const previousElementChildren = item.previousElementSibling?.children;
+        if (previousElementChildren) previousElementChildren[previousElementChildren.length - 1]?.classList?.add(`${SKILLS_CLASSNAME}__title--animating`)
+      }
+
+    }, TIME_OUT_DIFFERENTIAL * i);
+  }
+
+  (e.target as HTMLElement)?.classList?.toggle(SKILLS_SECTION_OPEN_CLASSNAME);
+}
