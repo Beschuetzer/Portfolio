9d6371778a9597db8d0691f3f8f63ed41a9f7c50
Adam <adam.j.major@gmail.com>
2021-06-11 21:20:10 -0700

    Convert SkillsItem to PureComponent

--- frontend/src/components/Skills/SkillsItem.tsx
+++ frontend/src/components/Skills/SkillsItem.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useRef, useState, useEffect } from "react";
+import React, { RefObject } from "react";
 import { connect } from "react-redux";
 import { clickSkill } from "../../actions"
 import SkillsItemSectionLabels from "./SkillsItemSectionLabels";
@@ -14,19 +13,48 @@ interface SkillsItemProps {
 	clickSkill: (value: HTMLElement) => void;
 }
 
-const SkillsItem: React.FC<SkillsItemProps> = ({
-	title,
-	percent,
-	href,
-	clickSkill,
-	hours,
-	label,
-}) => {
-	const skillsPopupDiv = document.querySelector("#skillsPopup");
-	const percentDiv = useRef<any>(null);
-	const [isDivSet, setIsDivSet] = useState(false);
+interface SkillsItemState {
+	isDivSet: boolean,
+}
 
-	const onParagraphClick = (e: MouseEvent) => {
+class SkillsItem extends React.PureComponent<SkillsItemProps, SkillsItemState> {
+	skillsPopupDiv: HTMLElement;
+	percentDiv: RefObject<HTMLElement>;
+	title: string;
+	percent: string;
+	href: string;
+	clickSkill: (value: HTMLElement) => void;
+	hours: string;
+	label: SkillsItemLabel;
+
+	constructor(
+		props: any,
+	) {
+		super(props);
+		
+		this.skillsPopupDiv = document.querySelector("#skillsPopup")!;
+		this.percentDiv = React.createRef();
+		this.title = this.props.title;
+		this.percent = this.props.percent;
+		this.href = this.props.href;
+		this.clickSkill = this.props.clickSkill;
+		this.hours = this.props.hours as any;
+		this.label = this.props.label;
+
+		this.state = {
+			isDivSet: false,
+		}
+	}
+
+	componentDidMount() {
+	}
+
+	componentDidUpdate() {
+		(this.percentDiv?.current as any).style.width = `${this.percent}%`;
+		this.setState({isDivSet: true});
+	}
+
+	onParagraphClick = (e: MouseEvent) => {
 		if (
 			(
 				(e.target as HTMLElement)?.parentNode?.parentNode as any
@@ -34,52 +62,49 @@ const SkillsItem: React.FC<SkillsItemProps> = ({
 		)
 			return;
 
-		skillsPopupDiv?.classList?.toggle(`${SKILLS_CLASSNAME}-popup--active`);
-		clickSkill(e.target as HTMLElement);
+		this.skillsPopupDiv?.classList?.toggle(`${SKILLS_CLASSNAME}-popup--active`);
+		this.clickSkill(e.target as HTMLElement);
 	};
 
-	useEffect(() => {
-		percentDiv.current.style.width = `${percent}%`;
-		setIsDivSet(true);
-	}, [percentDiv, percent, isDivSet]);
-
-	return (
-		<React.Fragment>
-			<li className={`${SKILLS_CLASSNAME}__item`}>
-				<svg className={`${SKILLS_CLASSNAME}__section-svg`}>
-					<use xlinkHref="/sprite.svg#icon-circle"></use>
-				</svg>
-				{href ? (
-					<a
-						target="_blank"
-						rel="noreferrer"
-						className={`${SKILLS_CLASSNAME}__title`}
-						href={href}>
-						{title}:
-					</a>
-				) : (
-					<div
-						onClick={(e: any) => onParagraphClick(e)}
-						className={`${SKILLS_CLASSNAME}__title`}>
-						{title}:
+	render() {
+		return (
+			<React.Fragment>
+				<li className={`${SKILLS_CLASSNAME}__item`}>
+					<svg className={`${SKILLS_CLASSNAME}__section-svg`}>
+						<use xlinkHref="/sprite.svg#icon-circle"></use>
+					</svg>
+					{this.href ? (
+						<a
+							target="_blank"
+							rel="noreferrer"
+							className={`${SKILLS_CLASSNAME}__title`}
+							href={this.href}>
+							{this.title}:
+						</a>
+					) : (
+						<div
+							onClick={(e: any) => this.onParagraphClick(e)}
+							className={`${SKILLS_CLASSNAME}__title`}>
+							{this.title}:
+						</div>
+					)}
+				</li>
+				<div className={`${SKILLS_CLASSNAME}__percent-outer`}>
+					<SkillsItemSectionLabels label={this.label} />
+					<div ref={this.percentDiv as any} className={`${SKILLS_CLASSNAME}__percent-inner`}>
+						<div className={`${SKILLS_CLASSNAME}__hours`}>
+							{this.hours ? `~ ${this.hours} hours` : null}
+						</div>
 					</div>
-				)}
-			</li>
-			<div className={`${SKILLS_CLASSNAME}__percent-outer`}>
-				<SkillsItemSectionLabels label={label} />
-				<div ref={percentDiv} className={`${SKILLS_CLASSNAME}__percent-inner`}>
-					<div className={`${SKILLS_CLASSNAME}__hours`}>
-						{hours ? `~ ${hours} hours` : null}
-					</div>
+					<div className={`${SKILLS_CLASSNAME}__percent-outer-left`}></div>
+					<div className={`${SKILLS_CLASSNAME}__percent-outer-center`}></div>
+					<div className={`${SKILLS_CLASSNAME}__percent-outer-right`}></div>
 				</div>
-				<div className={`${SKILLS_CLASSNAME}__percent-outer-left`}></div>
-				<div className={`${SKILLS_CLASSNAME}__percent-outer-center`}></div>
-				<div className={`${SKILLS_CLASSNAME}__percent-outer-right`}></div>
-			</div>
-		</React.Fragment>
-	);
+			</React.Fragment>
+		);
+	}
 };
 
 export default connect(null, {
 	clickSkill,
-})(SkillsItem);
+})(SkillsItem as any);
