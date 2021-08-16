b8cacd9e8c5d1be85580e14ed59cf2f8e1fab555
Adam <adam.j.major@gmail.com>
2021-08-16 08:22:19 -0500

    Change where SiteNav and Github buttons reside in header

--- frontend/src/components/navbar/SiteNav/SiteNav.tsx
+++ frontend/src/components/navbar/SiteNav/SiteNav.tsx
@@ -143,7 +143,7 @@ const SiteNav: React.FC<SiteNavProps> = ({
 	}, [currentUrl]);
 
 	useEffect(() => {
-		setHeaderHeightOnViewPortChange(viewPortWidth, setHeaderHeight);
+		// setHeaderHeightOnViewPortChange(viewPortWidth, setHeaderHeight);
 		resetPageNavMinWidth(viewPortWidth);
 		setHeaderHeightCSSPropertyValue();
 	}, [viewPortWidth, setHeaderHeight]);
