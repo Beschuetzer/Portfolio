221dc9d9728588244fdc7d260955e11b9cbfa09e
Adam <62818816+Beschuetzer@users.noreply.github.com>
2023-01-25 18:49:53 -0600

    fix changing width in resume breaks work history layout (grid) and change font-size down to _carousel.scss

--- frontend/src/pages/resume/ReferenceItem.tsx
+++ frontend/src/pages/resume/ReferenceItem.tsx
@@ -23,22 +23,22 @@ const ReferenceItem: React.FC<ReferenceItemProps> = ({
 			<div className={`${REFERENCES_CLASSNAME}__name`}>
 				{href ? (
 					<React.Fragment>
-						<span className={`${REFERENCES_CLASSNAME}__number`}>{number}.</span>
+						<span className={`${REFERENCES_CLASSNAME}__number`}>{number}).</span>
 						<a
 							target="_blank"
 							rel="noreferrer"
 							href={href}
 							className={`${REFERENCES_CLASSNAME}__name-text ${REFERENCES_CLASSNAME}__name-link`}>
-							{name}:
+							{name}
 						</a>
 					</React.Fragment>
 				) : (
 					<React.Fragment>
 						<span
 							className={`${REFERENCES_CLASSNAME}__number ${REFERENCES_CLASSNAME}__number--no-link`}>
-							{number}.
+							{number}).
 						</span>
-						<div className={`${REFERENCES_CLASSNAME}__name-text`}>{name}:</div>
+						<div className={`${REFERENCES_CLASSNAME}__name-text`}>{name}</div>
 					</React.Fragment>
 				)}
 			</div>
