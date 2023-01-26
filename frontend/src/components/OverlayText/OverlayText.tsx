c5c6a15bce03962817c00989be9a196da8b0af6a
Adam <62818816+Beschuetzer@users.noreply.github.com>
2023-01-26 09:13:11 -0600

    add close button to OverlayText

--- frontend/src/components/OverlayText/OverlayText.tsx
+++ frontend/src/components/OverlayText/OverlayText.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties, useRef, useEffect } from 'react'
+import React, { CSSProperties, useRef, useEffect, useState } from 'react'
 import { reconcileStyles } from './utils';
 
 type Positions = "relative" | 'static' | 'absolute' | 'fixed';
@@ -31,14 +31,26 @@ const OverlayText: React.FC<OverlayTextProps> = ({
 
   const reference = useRef<any>(null);
   let stylesToUse = reconcileStyles(styles as CSSProperties);
+  const [isVisible, setIsVisible] = useState(true);
+
+  function onClick() {
+    setIsVisible((isVisible) => !isVisible);
+  }
+
   useEffect(() => {
     //need to set parent to position relative inline style
     (reference.current.parentNode?.style as CSSProperties).position = positionType;
   }, [positionType])
 
+  if (!isVisible) return null;
   return (
     <div ref={reference} className={`${cssClassname}`} style={stylesToUse ? stylesToUse : null}>
-      <h3 className={`${cssClassname}-title` }>{titleText}</h3>
+      <div className={`${cssClassname}-title-container`} onClick={onClick}>
+        <h3 className={`${cssClassname}-title` }>{titleText}</h3>
+        <svg className={`${cssClassname}-title-close`}>
+          <use xlinkHref="/sprite.svg#icon-close"/>
+        </svg>
+      </div>
       <div className={`${cssClassname}-content` }>
         {children}
       </div>
