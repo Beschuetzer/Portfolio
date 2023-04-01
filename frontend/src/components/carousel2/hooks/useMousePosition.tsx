1a70f62fd253768402a3fbb5a5c8208ae192a526
Adam <62818816+Beschuetzer@users.noreply.github.com>
2023-04-01 11:54:12 -0500

    Start cursor hiding logic

---
+++ frontend/src/components/carousel2/hooks/useMousePosition.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useRef } from "react";
+import { Point } from "../types";
+
+/*
+*Returns a ref that tracks the mouse position as it moves
+*/
+export const useMousePosition = () => {
+    const mousePositionRef = useRef<Point>({} as Point);
+
+    useEffect(() => {
+        function updatePosition(e: MouseEvent) {
+            const { clientX, clientY} = e;
+            if (mousePositionRef.current) {
+                mousePositionRef.current.x = clientX;
+                mousePositionRef.current.y = clientY;
+            }
+        }
+
+        window.addEventListener('mousemove', updatePosition);
+        return () => {
+            window.removeEventListener('mousemove', updatePosition);
+        }
+    }, [])
+
+    return mousePositionRef;
+}
