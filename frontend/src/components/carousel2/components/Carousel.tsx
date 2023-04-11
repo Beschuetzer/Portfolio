36f7b4f6d2629bbbe49adec71019edfe09260a29
Adam <62818816+Beschuetzer@users.noreply.github.com>
2023-04-11 08:01:55 -0500

    Add CarouselContent in preparation for page navigation stuff

--- frontend/src/components/carousel2/components/Carousel.tsx
+++ frontend/src/components/carousel2/components/Carousel.tsx
@@ -1,14 +1,12 @@
-import { CSSProperties, useCallback, useEffect, useRef, useState } from 'react'
-import { CURRENT_ITEM_INDEX_INITIAL, CURRENT_PAGE_INITIAL, useCarouselContext } from '../context';
+import { CSSProperties, useEffect, useRef } from 'react'
+import { CURRENT_ITEM_INDEX_INITIAL, useCarouselContext } from '../context';
 import { getClassname, getGuid, toggleFullScreenMode } from '../utils';
 import { CarouselInstanceProvider } from './CarouselInstanceProvider';
-import { CarouselItemProps, CarouselItem } from './CarouselItem';
+import { CarouselItemProps } from './CarouselItem';
 import { CarouselOptions, CarouselSvgHrefs } from '../types';
-import { CAROUSEL_ITEM_SIZE_DEFAULT, CAROUSEL_ITEM_SPACING_DEFAULT } from '../constants';
-import { CarouselDots } from './CarouselDots';
-import { CarouselArrowButton } from './CarouselArrowButton';
+import { CarouselContent } from './CarouselContent';
 
-type CarouselProps = {
+export type CarouselProps = {
 	style?: CSSProperties;
 	/*
 	* if undefined, the default css version for each button will be used
@@ -19,44 +17,22 @@ type CarouselProps = {
 	onItemChange?: (isViewerOpen?: boolean) => void;
 }
 
-export const Carousel = ({
-	style = {},
-	svgHrefs = {},
-	items,
-	options,
-	onItemChange = () => null,
-}: CarouselProps) => {
+export const Carousel = (props: CarouselProps) => {
 	//#region Init
+	const {
+		style = {},
+		svgHrefs = {},
+		items,
+		options,
+		onItemChange = () => null,
+	} = props;
 	const { currentItemIndex, currentItems, setCurrentItems, currentCarouselId, itemViewerRef } = useCarouselContext();
 	const idRef = useRef<string>(getGuid());
 	const carouselContainerRef = useRef<HTMLDivElement>();
-	const hasCalculatedItemSpacingRef = useRef(false);
-	const [hasForcedRender, setHasForcedRender] = useState(false); //used to force layout calculation initially
-	const [interItemSpacing, setInterItemSpacing] = useState(`${options?.thumbnail?.itemSpacing || CAROUSEL_ITEM_SPACING_DEFAULT}px`);
-	const [currentPage, setCurrentPage] = useState(CURRENT_PAGE_INITIAL);
 	const isCurrentCarousel = currentCarouselId === idRef.current;
 	//#endregion
 
-	//#region Functions/Handlers
-	const getInterItemSpacing = useCallback(() => {
-		//if there is itemSpacing is defined, the dynamic behavior is disabled
-		if (options?.thumbnail?.itemSpacing) return `${options?.thumbnail?.itemSpacing}px`;
-		const containerWidth = carouselContainerRef.current?.getBoundingClientRect()?.width || 0;
-		const itemSize = options?.thumbnail?.size || CAROUSEL_ITEM_SIZE_DEFAULT;
-		const numberOfItemsThatCanFit = Math.floor(containerWidth / itemSize);
-		const numberOfGaps = numberOfItemsThatCanFit - 1;
-		const remainingSpace = containerWidth - (numberOfItemsThatCanFit * itemSize);
-		const newInterItemSpacing = (remainingSpace / numberOfGaps);
-		return `${newInterItemSpacing || CAROUSEL_ITEM_SPACING_DEFAULT}px`;
-	}, [options?.thumbnail, carouselContainerRef, CAROUSEL_ITEM_SPACING_DEFAULT]);
-	//#endregion
-
 	//#region Side Fx
-	//have to reset hasCalculatedItemSpacingRef if the dependencies for the getInterItemSpacing callback change
-	useEffect(() => {
-		hasCalculatedItemSpacingRef.current = false;
-	}, [options?.thumbnail, carouselContainerRef, CAROUSEL_ITEM_SPACING_DEFAULT, hasCalculatedItemSpacingRef])
-
 	useEffect(() => {
 		if (!isCurrentCarousel) return;
 		onItemChange && onItemChange(!!currentItems?.[currentItemIndex] || false);
@@ -67,22 +43,9 @@ export const Carousel = ({
 		if (!isCurrentCarousel || currentItems?.length === items?.length || currentItemIndex === CURRENT_ITEM_INDEX_INITIAL) return;
 		setCurrentItems(items);
 	}, [isCurrentCarousel, currentCarouselId, currentItems, items, currentItemIndex, CURRENT_ITEM_INDEX_INITIAL])
-
-	useEffect(() => {
-		if (!hasForcedRender) return setHasForcedRender(true);
-		else if (hasCalculatedItemSpacingRef.current) return;
-		hasCalculatedItemSpacingRef.current = true;
-		setInterItemSpacing(getInterItemSpacing());
-	}, [hasForcedRender, setHasForcedRender, setInterItemSpacing, getInterItemSpacing, currentItems, hasCalculatedItemSpacingRef])
 	//#endregion
 
 	//#region JSX
-	const interItemSpacingStyle = {
-		columnGap: interItemSpacing,
-	} as CSSProperties
-	const containerStyle = {
-		...interItemSpacingStyle,
-	}
 	return (
 		<CarouselInstanceProvider
 			carouselContainerRef={carouselContainerRef as any}
@@ -90,21 +53,7 @@ export const Carousel = ({
 			options={options}
 			svgHrefInstance={svgHrefs}>
 			<div ref={carouselContainerRef as any} className={getClassname({ elementName: "" })} style={style}>
-				<div style={containerStyle} className={getClassname({ elementName: "container" })}>
-					{
-						items.map((item, index) => <CarouselItem key={index} index={index} {...item} />)
-					}
-				</div>
-				<div className={getClassname({ elementName: "navigation" })}>
-					<CarouselArrowButton svgHrefs={svgHrefs} direction={"left"} onClick={() => console.log('left clicked')} />
-					<CarouselDots
-						svgHrefs={svgHrefs}
-						items={items || []}
-						setCurrentPage={setCurrentPage}
-						currentPage={currentPage}
-					/>
-					<CarouselArrowButton svgHrefs={svgHrefs} direction={"right"} onClick={() => console.log('right clicked')} />
-				</div>
+				<CarouselContent {...props} carouselContainerRef={carouselContainerRef} />
 			</div>
 		</CarouselInstanceProvider>
 	)
