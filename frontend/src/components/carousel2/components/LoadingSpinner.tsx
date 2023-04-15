1b2657b8b81e5dfa4bdacfcd253c0154bc372079
Adam <62818816+Beschuetzer@users.noreply.github.com>
2023-04-14 21:35:59 -0500

    Finish ring customization options

--- frontend/src/components/carousel2/components/LoadingSpinner.tsx
+++ frontend/src/components/carousel2/components/LoadingSpinner.tsx
@@ -1,13 +1,37 @@
 import { ReactNode } from "react";
 
-type LoadingSpinnerProps = {
-	description?: string;
-	show?: boolean;
-	type?: 'spinner' | 'ring'
+type RingOptions = {
+	/*
+	*default is 100px; The length and width of the container
+	*/
+	containerLength?: number;
+	/*
+	*Default is 64px; How big the circle is
+	*/
+	radius?: number;
+	/*
+	*Default is 8px;  How thick the line is
+	*/
+	width?: number;
 }
 
+type LoadingSpinnerCommonProps = {
+	description?: string;
+	show?: boolean;
+}
+type LoadingSpinnerProps =  {
+	type?: 'ring',
+	options?: RingOptions;
+} & LoadingSpinnerCommonProps | {
+	type?: 'spinner',
+	options?: {};
+} & LoadingSpinnerCommonProps
+
+
+const RING_RADIUS_DEFAULT = 64;
 export const LoadingSpinner = ({
 	description = '',
+	options = {},
 	show = false,
 	type = 'ring',
 }: LoadingSpinnerProps) => {
@@ -32,14 +56,32 @@ export const LoadingSpinner = ({
 					</div>
 				);
 			case "ring":
+				const { radius, width, containerLength } = options as RingOptions;
+				const isContainerLengthLessThanRadius = containerLength && containerLength <= (radius || RING_RADIUS_DEFAULT);
+				const widthStyle = containerLength ? {
+					width: containerLength,
+					height: containerLength,
+				} as React.CSSProperties : {}
+				const divRadiusStyle = radius || isContainerLengthLessThanRadius ? {
+					width: Math.min((radius || Number.MAX_SAFE_INTEGER), containerLength || Number.MAX_SAFE_INTEGER),
+					height: Math.min(radius || Number.MAX_SAFE_INTEGER, containerLength || Number.MAX_SAFE_INTEGER),
+				} as React.CSSProperties : {}
+				const divSizeStyle = width || containerLength ? {
+					margin: width ? width : isContainerLengthLessThanRadius ? containerLength / 4 : 4,
+					border: `${width ? width : isContainerLengthLessThanRadius ? containerLength / 4 : 4}px solid #fff`,
+					borderTopColor: `#fff`,
+					borderRightColor: `transparent`,
+					borderBottomColor: `transparent`,
+					borderLeftColor: `transparent`,
+				} as React.CSSProperties : {}
 				return (
 					<>
 						{content}
-						<div className="lds-ring">
-							<div />
-							<div />
-							<div />
-							<div />
+						<div className="lds-ring" style={widthStyle}>
+							<div style={{...divRadiusStyle, ...divSizeStyle}}/>
+							<div style={{...divRadiusStyle, ...divSizeStyle}}/>
+							<div style={{...divRadiusStyle, ...divSizeStyle}}/>
+							<div style={{...divRadiusStyle, ...divSizeStyle}}/>
 						</div>
 					</>
 				);
