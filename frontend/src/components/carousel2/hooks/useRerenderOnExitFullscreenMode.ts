128364c565968e745b40e82987a969d78a4f0c21
Adam <62818816+Beschuetzer@users.noreply.github.com>
2023-05-07 08:39:15 -0500

    add note

--- frontend/src/components/carousel2/hooks/useRerenderOnExitFullscreenMode.ts
+++ frontend/src/components/carousel2/hooks/useRerenderOnExitFullscreenMode.ts
@@ -3,6 +3,9 @@ import { useCarouselContext } from "../context";
 import { useRenderCount as useRenderCountRef } from "./useRenderCountRef";
 
 //this is a hack to get the image/video to re-render after exiting full screen when 'itemDisplayLocation' is not 'none'
+//the issue seems to stem from the fact that StylingLogic.maxHeightNonDefaultItemDisplayLocation
+//depends on the toolbar ref but the toolbar is a child of 
+//CarouselVideo/Image, so the style is calculated before the child has finished rendering
 export const useRerenderOnExitFullscreenMode = () => {
     const { isFullscreenMode } = useCarouselContext();
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
