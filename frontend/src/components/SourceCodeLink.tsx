51ecdb86d53fd63c893c861dcd0f33c4cf9cde52
Adam <62818816+Beschuetzer@users.noreply.github.com>
2023-11-03 10:27:27 -0500

    Clean up Bridge.tsx and SourceCodeLink.tsx

--- frontend/src/components/SourceCodeLink.tsx
+++ frontend/src/components/SourceCodeLink.tsx
@@ -1,37 +1,39 @@
-import React, { useState } from "react";
-import { useAppSelector } from "../hooks";
+import React from "react";
 import { useBridgeSectionTransitionHiding } from "../hooks/useBridgeSectionTransitionHiding";
-import { useGetBridgeSections } from "../hooks/useGetBridgeSections";
-import { BridgeSectionHidingLogic } from "../pages/examples/bridge/BridgeSectionHidingLogic";
-import { currentBridgeSectionSelector, clickedBridgeInfoButtonCountSelector } from "../slices";
 
-interface  SourceCodeLinkProps { 
-  href: string,
-  blockName?: string,
-  msg?: string,
-	className?: string,
+interface SourceCodeLinkProps {
+  href: string;
+  blockName?: string;
+  msg?: string;
+  className?: string;
 }
 
-export const SourceCodeLink: React.FC<SourceCodeLinkProps> = ({ 
+export const SourceCodeLink: React.FC<SourceCodeLinkProps> = ({
   href,
   blockName = "source-link",
   msg = "Code",
-	className,
+  className,
 }) => {
-	const defaultClassname = `${blockName}__source`;
-	const isHiddenDuringBridgeSectionTransition = useBridgeSectionTransitionHiding();
-	
-	if (isHiddenDuringBridgeSectionTransition) return null;
-	return (
-		<a
-			target="_blank"
-			rel="noreferrer"
-			href={href}
-			className={className ? `${className} ${defaultClassname}` : defaultClassname}>
-			<svg aria-hidden="true" className={`${blockName}__source-svg`}>
-				<use xlinkHref="/sprite.svg#icon-code"></use>
-			</svg>
-			<span aria-hidden="true" className={`${blockName}__source-label`}>{msg}</span>
-		</a>
-	);
+  const defaultClassname = `${blockName}__source`;
+  const isHiddenDuringBridgeSectionTransition =
+    useBridgeSectionTransitionHiding();
+
+  if (isHiddenDuringBridgeSectionTransition) return null;
+  return (
+    <a
+      target="_blank"
+      rel="noreferrer"
+      href={href}
+      className={
+        className ? `${className} ${defaultClassname}` : defaultClassname
+      }
+    >
+      <svg aria-hidden="true" className={`${blockName}__source-svg`}>
+        <use xlinkHref="/sprite.svg#icon-code"></use>
+      </svg>
+      <span aria-hidden="true" className={`${blockName}__source-label`}>
+        {msg}
+      </span>
+    </a>
+  );
 };
