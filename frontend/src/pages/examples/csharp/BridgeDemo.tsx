757f50a4ba24417c2680e2ea33403bed40ab86fe
Adam <62818816+Beschuetzer@users.noreply.github.com>
2023-11-11 07:07:24 -0600

    Fix issue with images in downloader carousel

--- frontend/src/pages/examples/csharp/BridgeDemo.tsx
+++ frontend/src/pages/examples/csharp/BridgeDemo.tsx
@@ -28,178 +28,206 @@ const sectionNames = [
 interface BridgeDemoProps {}
 
 export const BridgeDemo: React.FC<BridgeDemoProps> = () => {
-  return <CSharpLayout sections={[
-  {
-    name: sectionNames[0],
-    pageName: C_SHARP_CLASSNAME,
-    children: [
-      <React.Fragment>
-        <CSharpCardSection>
-          <p>
-            Since there is no AI currently implemented, runing a demo of A#Maj
-            Bridge requires one to open two different browsers with two tabs in
-            each browser (one of which has to be a "Private Browsing" tab or
-            "Incognito" tab).
-          </p>
-          <ol>
-            <li>Two separate browsers (neither can be IE)</li>
-            <li>
-              Browsers must have a way to create independent sessions (e.g.
-              "Private Window" in Firefox and "Incognito Mode" in Crome)
-            </li>
-          </ol>
-        </CSharpCardSection>
-      </React.Fragment>,
-    ],
-  },
-  {
-    name: sectionNames[1],
-    pageName: C_SHARP_CLASSNAME,
-    children: [
-      <React.Fragment>
-        <CSharpCardSection>
-          <ol>
-            <li>Open both browsers</li>
-            <li>
-              Open one private browsing window for each browser (four windows
-              total)
-            </li>
-            <li>
-              Navigate to{" "}
-              <EmbeddedLink
-                isLocal={false}
-                href={`${LIVE_BRIDGE_URL}/login`}
-                addSpaces={false}
-              >
-                here
-              </EmbeddedLink>
-              &nbsp;in each window
-            </li>
-            <li>
-              In each window login using one of the four usernames below and the
-              password (each window must login with a separate username
-              otherwise game-play will be impossible due to security reasons):
-              <ul className="padding-top-1 padding-bottom-1">
-                <li>Username 1: 'Test'</li>
-                <li>Username 2: 'TEST'</li>
-                <li>Username 3: 'tesT'</li>
-                <li>Username 4: 'test'</li>
-                <li>Password: 'test'</li>
-              </ul>
-            </li>
-            <li>
-              Explore the site and try creating a lobby to start a game to see
-              the actual gameplay
-            </li>
-            <li>See below section for videos explaining the above info</li>
-          </ol>
-        </CSharpCardSection>
-      </React.Fragment>,
-    ],
-  },
-  {
-    hasCarousel: false,
-    name: sectionNames[2],
-    pageName: C_SHARP_CLASSNAME,
-    children: [
-      <Carousel
-        options={{
-          thumbnail: {
-            descriptionOverlay: {
-              hideDescriptionOverlayUnlessHovered: false,
-              background: {
-                solid: {
-                  color: getComputedStyleCustom("--color-primary-2"),
-                  opacity: 0.9,
+  return (
+    <CSharpLayout
+      sections={[
+        {
+          name: sectionNames[0],
+          pageName: C_SHARP_CLASSNAME,
+          children: [
+            <React.Fragment>
+              <CSharpCardSection>
+                <p>
+                  Since there is no AI currently implemented, runing a demo of
+                  A#Maj Bridge requires one to open two different browsers with
+                  two tabs in each browser (one of which has to be a "Private
+                  Browsing" tab or "Incognito" tab).
+                </p>
+                <ol>
+                  <li>Two separate browsers (neither can be IE)</li>
+                  <li>
+                    Browsers must have a way to create independent sessions
+                    (e.g. "Private Window" in Firefox and "Incognito Mode" in
+                    Crome)
+                  </li>
+                </ol>
+              </CSharpCardSection>
+            </React.Fragment>,
+          ],
+        },
+        {
+          name: sectionNames[1],
+          pageName: C_SHARP_CLASSNAME,
+          children: [
+            <React.Fragment>
+              <CSharpCardSection>
+                <ol>
+                  <li>Open both browsers</li>
+                  <li>
+                    Open one private browsing window for each browser (four
+                    windows total)
+                  </li>
+                  <li>
+                    Navigate to{" "}
+                    <EmbeddedLink
+                      isLocal={false}
+                      href={`${LIVE_BRIDGE_URL}/login`}
+                      addSpaces={false}
+                    >
+                      here
+                    </EmbeddedLink>
+                    &nbsp;in each window
+                  </li>
+                  <li>
+                    In each window login using one of the four usernames below
+                    and the password (each window must login with a separate
+                    username otherwise game-play will be impossible due to
+                    security reasons):
+                    <ul className="padding-top-1 padding-bottom-1">
+                      <li>Username 1: 'Test'</li>
+                      <li>Username 2: 'TEST'</li>
+                      <li>Username 3: 'tesT'</li>
+                      <li>Username 4: 'test'</li>
+                      <li>Password: 'test'</li>
+                    </ul>
+                  </li>
+                  <li>
+                    Explore the site and try creating a lobby to start a game to
+                    see the actual gameplay
+                  </li>
+                  <li>
+                    See below section for videos explaining the above info
+                  </li>
+                </ol>
+              </CSharpCardSection>
+            </React.Fragment>,
+          ],
+        },
+        {
+          hasCarousel: false,
+          name: sectionNames[2],
+          pageName: C_SHARP_CLASSNAME,
+          children: [
+            <Carousel
+              options={{
+                thumbnail: {
+                  size: 200,
+                  descriptionOverlay: {
+                    hideDescriptionOverlayUnlessHovered: false,
+                    background: {
+                      gradient: {
+                        start: {
+                          color: getComputedStyleCustom("--color-primary-3"),
+                          opacity: 1,
+                        },
+                        end: {
+                          color: getComputedStyleCustom("--color-primary-1"),
+                          opacity: 1
+                        } ,
+                        angle: 270,
+                      },
+                    },
+                  },
+                  spacingStrategy: "max",
                 },
-              },
-              textColor: getComputedStyleCustom("--color-primary-4"),
-            },
-            spacingStrategy: "max",
-          },
-          styling: {
-            container: {
-              padding: {
-                top: 14,
-                bottom: 14,
-                left: 28,
-                right: 28,
-              },
-            },
-          },
-        }}
-        items={[
-          {
-            srcMain: video1,
-            srcThumbnail: video1Thumbnail,
-            description: "Setting Up",
-            modal: {
-              sections: [
-                {
-                  title: "Four Windows at Once",
-                  text: "This video show you how to open Chrome and Firefox and create private session windows",
+                layout: {
+                  itemDisplayLocation: "above",
                 },
-              ],
-            },
-            video: {
-              sections: [
-                ["Opening First Browser", 6500],
-                ["Opening Icognito Mode in First Browser ", 4500],
-                ["Opening Second Browser", 4200],
-                ["Opening Icognito Mode in Second Browser ", 0],
-              ],
-            },
-          },
-          {
-            srcMain: video2,
-            srcThumbnail: video2Thumbnail,
-            description: "Logging In",
-            modal: {
-              sections: [
-                {
-                  title: "Test is the Password",
-                  text: " Notice how each window uses a separate username but the password is the same.",
+                styling: {
+                  colorTheme: {
+                    colorOne: getComputedStyleCustom("--color-primary-1"),
+                    colorTwo: getComputedStyleCustom("--color-primary-2"),
+                    colorThree: getComputedStyleCustom("--color-primary-3"),
+                    colorFour: getComputedStyleCustom("--color-primary-3"),
+                    colorFive: getComputedStyleCustom("--color-primary-4"),
+                  },
+                  container: {
+                    padding: {
+                      top: 14,
+                      bottom: 14,
+                      left: 28,
+                      right: 28,
+                    },
+                  },
                 },
-              ],
-            },
-            video: {
-              sections: [
-                ["Logging in as 'test'", ""],
-                ["Logging in as 'Test'", "8:00"],
-                ["Logging in as 'TEST'", "12:00"],
-                ["Logging in as 'tesT'", "15:00"],
-              ],
-            },
-          },
-          {
-            srcMain: video3,
-            srcThumbnail: video3Thumbnail,
-            description: "Starting a Game",
-            modal: {
-              sections: [
+              }}
+              items={[
                 {
-                  title: "Create a Lobby",
-                  text: "After logging in, create a lobby by entering a name and clicking 'Go'.",
+                  srcMain: video1,
+                  srcThumbnail: video1Thumbnail,
+                  description: "Setting Up",
+                  modal: {
+                    sections: [
+                      {
+                        title: "Four Windows at Once",
+                        text: "This video show you how to open Chrome and Firefox and create private session windows",
+                      },
+                    ],
+                  },
+                  video: {
+                    sections: [
+                      ["Opening First Browser", 6500],
+                      ["Opening Icognito Mode in First Browser ", 4500],
+                      ["Opening Second Browser", 4200],
+                      ["Opening Icognito Mode in Second Browser ", 0],
+                    ],
+                    autoPlay: false,
+                  },
                 },
                 {
-                  title: "Join the Lobby",
-                  text: "Clicking 'Go' with a lobby name that is open will join that lobby.",
+                  srcMain: video2,
+                  srcThumbnail: video2Thumbnail,
+                  description: "Logging In",
+                  modal: {
+                    sections: [
+                      {
+                        title: "Test is the Password",
+                        text: " Notice how each window uses a separate username but the password is the same.",
+                      },
+                    ],
+                  },
+                  video: {
+                    sections: [
+                      ["Logging in as 'test'", ""],
+                      ["Logging in as 'Test'", "8:00"],
+                      ["Logging in as 'TEST'", "12:00"],
+                      ["Logging in as 'tesT'", "15:00"],
+                    ],
+                    autoPlay: false,
+                  },
                 },
-              ],
-            },
-            video: {
-              sections: [
-                ["Creating a Lobby", ""],
-                ["Setting up the Lobby", "15:00"],
-                ["Starting the Game", "21:00"],
-                ["Bidding", "24:00"],
-              ],
-            },
-          },
-        ]}
-      />,
-    ],
-  },
-]} 
-pageName={BRIDGE_DEMO_PAGE_NAME} />;
+                {
+                  srcMain: video3,
+                  srcThumbnail: video3Thumbnail,
+                  description: "Starting a Game",
+                  modal: {
+                    sections: [
+                      {
+                        title: "Create a Lobby",
+                        text: "After logging in, create a lobby by entering a name and clicking 'Go'.",
+                      },
+                      {
+                        title: "Join the Lobby",
+                        text: "Clicking 'Go' with a lobby name that is open will join that lobby.",
+                      },
+                    ],
+                  },
+                  video: {
+                    sections: [
+                      ["Creating a Lobby", ""],
+                      ["Setting up the Lobby", "15:00"],
+                      ["Starting the Game", "21:00"],
+                      ["Bidding", "24:00"],
+                    ],
+                    autoPlay: false,
+                  },
+                },
+              ]}
+            />,
+          ],
+        },
+      ]}
+      pageName={BRIDGE_DEMO_PAGE_NAME}
+    />
+  );
 };
--- frontend/src/pages/examples/csharp/Downloader.tsx
+++ frontend/src/pages/examples/csharp/Downloader.tsx
@@ -41,181 +41,187 @@ export const Downloader: React.FC<DownloaderProps> = () => {
   return (
     <CSharpLayout
       sections={[
-  {
-    name: sectionNames[0],
-    pageName: C_SHARP_CLASSNAME,
-    children: [
-      <React.Fragment>
-        <Quote author="Peter Drucker" className="padding-top-1">
-          Efficiency is doing better what is already being done.
-        </Quote>
-        <CSharpCardSection title="Why">
-          Websites like
-          <EmbeddedLink href={OC_REMIX_URL}>OCRemix</EmbeddedLink> and
-          <EmbeddedLink href={KH_INSIDER_URL}>
-            Kingdom Hearts Insider
-          </EmbeddedLink>
-          offer mp3 files for downloading.&nbsp;&nbsp;It can be tedious
-          downloading each file by hand, so I decided to create a c# app that
-          takes in a Regular Expression and recursively downloads files that
-          match the expression given.
-        </CSharpCardSection>
-        <CSharpCardSection title="How">
-          The app finds any links that are on the url provided and recursively
-          crawls any sub-links until it runs out of links. &nbsp;On each page,
-          it looks for download links that match the Regular Expression and adds
-          them to a download queue. &nbsp;It works best on sites like
-          <EmbeddedLink href={KH_INSIDER_URL}>
-            Kingdom Hearts Insider
-          </EmbeddedLink>
-          where each album page only has links to that specific album.
-        </CSharpCardSection>
-      </React.Fragment>,
-    ],
-  },
-  {
-    styles: {
-      position: "relative",
-    },
-    name: sectionNames[1],
-    pageName: C_SHARP_CLASSNAME,
-    children: [
-      <Carousel
-        options={{
-			thumbnail: {
-				descriptionOverlay: {
-					hideDescriptionOverlayUnlessHovered: false,
-					background: {
-						solid: {
-							color: getComputedStyleCustom('--color-primary-2'),
-							opacity: .9
-						},
-					},
-					textColor: getComputedStyleCustom('--color-primary-4')
-				},
-			},
-			styling: {
-				container: {
-					padding: {
-						top: 14,
-						bottom: 14,
-						left: 28,
-						right: 28,
-					 }
-				},
-				elements: {
-					all: {
-						fillColor: getComputedStyleCustom('--color-primary-4'),
-					}
-				}
-			}
-		}}
-        items={[
-          {
-            srcMain: problemVideo,
-			srcThumbnail: problemVideoThumbnail,
-            description: "The manual way of downloading",
-            modal: {
-              children: (
-                <div>
-                  <CSharpCardSection title="The Problem">
-                    Downloading new releases from{" "}
-                    <EmbeddedLink isLocal={false} href={OC_REMIX_URL}>
-                      ocremix.org
-                    </EmbeddedLink>{" "}
-                    is exciting, but having to download a few months worth is
-                    more of a chore.&nbsp;
-                  </CSharpCardSection>
-                  <CSharpCardSection title="Reality Check">
-                    This is what it takes to download one song.
-                  </CSharpCardSection>
-                </div>
-              ),
-            },
+        {
+          name: sectionNames[0],
+          pageName: C_SHARP_CLASSNAME,
+          children: [
+            <React.Fragment>
+              <Quote author="Peter Drucker" className="padding-top-1">
+                Efficiency is doing better what is already being done.
+              </Quote>
+              <CSharpCardSection title="Why">
+                Websites like
+                <EmbeddedLink href={OC_REMIX_URL}>OCRemix</EmbeddedLink> and
+                <EmbeddedLink href={KH_INSIDER_URL}>
+                  Kingdom Hearts Insider
+                </EmbeddedLink>
+                offer mp3 files for downloading.&nbsp;&nbsp;It can be tedious
+                downloading each file by hand, so I decided to create a c# app
+                that takes in a Regular Expression and recursively downloads
+                files that match the expression given.
+              </CSharpCardSection>
+              <CSharpCardSection title="How">
+                The app finds any links that are on the url provided and
+                recursively crawls any sub-links until it runs out of links.
+                &nbsp;On each page, it looks for download links that match the
+                Regular Expression and adds them to a download queue. &nbsp;It
+                works best on sites like
+                <EmbeddedLink href={KH_INSIDER_URL}>
+                  Kingdom Hearts Insider
+                </EmbeddedLink>
+                where each album page only has links to that specific album.
+              </CSharpCardSection>
+            </React.Fragment>,
+          ],
+        },
+        {
+          styles: {
+            position: "relative",
           },
-          {
-            srcMain: demoVideo,
-			srcThumbnail: demoVideoThumbnail,
-            description: "The automated way of downloading",
-            modal: {
-              children: (
-                <div>
-                  <CSharpCardSection title="Embedded Links">
-                    The downloader app not only downloads links it finds
-                    matching the given criteria, but will do the same for any
-                    embedded links it finds.
-                  </CSharpCardSection>
-                  <CSharpCardSection title="Buggy but functional">
-                    The download logs aren't always correct, but there's no
-                    denying the result.
-                  </CSharpCardSection>
-                </div>
-              ),
-            },
-          },
-          {
-            srcMain: img1,
-            srcThumbnail: img1Thumbnail,
-            description: "The User interface",
-          },
-          {
-            srcMain: img2,
-            srcThumbnail: img2Thumbnail,
-            description: "Options available",
-          },
-          {
-            srcMain: img3,
-            srcThumbnail: img3Thumbnail,
-            description: "Full-screen user interface when downloading",
-          },
-          {
-            srcMain: img4,
-            srcThumbnail: img4Thumbnail,
-            description: "Integrated file-renaming tool",
-          },
-          {
-            srcMain: img5,
-            srcThumbnail: img5Thumbnail,
-            description:
-              "A list of songs from ocremix.org. A pain to download manually...",
-          },
-          {
-            srcMain: img6,
-            srcThumbnail: img6Thumbnail,
-            description: "Result of using Downloader",
-          },
-        ]}
-      />,
-    ],
-  },
-  {
-    name: sectionNames[2],
-    pageName: C_SHARP_CLASSNAME,
-    children: [
-      <React.Fragment>
-        <CSharpCardSection title="Multi-threading">
-          Downloader uses three threads.&nbsp; One thread analyzes the html
-          looking for sub-links and urls matching the Regular Expression. If it
-          finds a link to download it adds it to the download queue, which is
-          handled by the second thread.&nbsp; The last thread handles the GUI
-          updates.
-        </CSharpCardSection>
-        <CSharpCardSection title="A Challenging Concept">
-          At this point in time (March-April 2020), I had never written an
-          app/script that used multiple threads.&nbsp; It took a few days to
-          firmly grasp the concept, but once I had it, I was able to do
-          everything I wanted to do, namely analyze html, download files, and
-          update the GUI all at the same time. I took this understanding and
-          applied it to
-          <EmbeddedLink isLocal={true} href={PLAYLIST_SYNCER_URL}>
-            another problem
-          </EmbeddedLink>
-          I was facing at the time.
-        </CSharpCardSection>
-      </React.Fragment>,
-    ],
-  },
-]}
+          name: sectionNames[1],
+          pageName: C_SHARP_CLASSNAME,
+          children: [
+            <Carousel
+              options={{
+                layout: {
+                  itemDisplayLocation: "above",
+                },
+                thumbnail: {
+                  size: 200,
+                  descriptionOverlay: {
+                    hideDescriptionOverlayUnlessHovered: false,
+                  },
+                },
+                styling: {
+                  colorTheme: {
+                    colorOne: getComputedStyleCustom("--color-primary-1"),
+                    colorTwo: getComputedStyleCustom("--color-primary-2"),
+                    colorThree: getComputedStyleCustom("--color-primary-3"),
+                    colorFour: getComputedStyleCustom("--color-primary-3"),
+                    colorFive: getComputedStyleCustom("--color-primary-4"),
+                  },
+                  container: {
+                    padding: {
+                      top: 14,
+                      bottom: 14,
+                      left: 28,
+                      right: 28,
+                    },
+                    backgroundColor: "white",
+                  },
+                  elements: {
+                    all: {
+                      fillColor: getComputedStyleCustom("--color-primary-1"),
+                    },
+                  },
+                },
+              }}
+              items={[
+                {
+                  srcMain: problemVideo,
+                  srcThumbnail: problemVideoThumbnail,
+                  description: "The manual way of downloading",
+                  modal: {
+                    children: (
+                      <div>
+                        <CSharpCardSection title="The Problem">
+                          Downloading new releases from{" "}
+                          <EmbeddedLink isLocal={false} href={OC_REMIX_URL}>
+                            ocremix.org
+                          </EmbeddedLink>{" "}
+                          is exciting, but having to download a few months worth
+                          is more of a chore.&nbsp;
+                        </CSharpCardSection>
+                        <CSharpCardSection title="Reality Check">
+                          This is what it takes to download one song.
+                        </CSharpCardSection>
+                      </div>
+                    ),
+                  },
+                },
+                {
+                  srcMain: demoVideo,
+                  srcThumbnail: demoVideoThumbnail,
+                  description: "The automated way of downloading",
+                  modal: {
+                    children: (
+                      <div>
+                        <CSharpCardSection title="Embedded Links">
+                          The downloader app not only downloads links it finds
+                          matching the given criteria, but will do the same for
+                          any embedded links it finds.
+                        </CSharpCardSection>
+                        <CSharpCardSection title="Buggy but functional">
+                          The download logs aren't always correct, but there's
+                          no denying the result.
+                        </CSharpCardSection>
+                      </div>
+                    ),
+                  },
+                },
+                {
+                  srcMain: img1,
+                  srcThumbnail: img1Thumbnail,
+                  description: "The User interface",
+                },
+                {
+                  srcMain: img2,
+                  srcThumbnail: img2Thumbnail,
+                  description: "Options available",
+                },
+                {
+                  srcMain: img3,
+                  srcThumbnail: img3Thumbnail,
+                  description: "Full-screen user interface when downloading",
+                },
+                {
+                  srcMain: img4,
+                  srcThumbnail: img4Thumbnail,
+                  description: "Integrated file-renaming tool",
+                },
+                {
+                  srcMain: img5,
+                  srcThumbnail: img5Thumbnail,
+                  description:
+                    "A list of songs from ocremix.org. A pain to download manually...",
+                },
+                {
+                  srcMain: img6,
+                  srcThumbnail: img6Thumbnail,
+                  description: "Result of using Downloader",
+                },
+              ]}
+            />,
+          ],
+        },
+        {
+          name: sectionNames[2],
+          pageName: C_SHARP_CLASSNAME,
+          children: [
+            <React.Fragment>
+              <CSharpCardSection title="Multi-threading">
+                Downloader uses three threads.&nbsp; One thread analyzes the
+                html looking for sub-links and urls matching the Regular
+                Expression. If it finds a link to download it adds it to the
+                download queue, which is handled by the second thread.&nbsp; The
+                last thread handles the GUI updates.
+              </CSharpCardSection>
+              <CSharpCardSection title="A Challenging Concept">
+                At this point in time (March-April 2020), I had never written an
+                app/script that used multiple threads.&nbsp; It took a few days
+                to firmly grasp the concept, but once I had it, I was able to do
+                everything I wanted to do, namely analyze html, download files,
+                and update the GUI all at the same time. I took this
+                understanding and applied it to
+                <EmbeddedLink isLocal={true} href={PLAYLIST_SYNCER_URL}>
+                  another problem
+                </EmbeddedLink>
+                I was facing at the time.
+              </CSharpCardSection>
+            </React.Fragment>,
+          ],
+        },
+      ]}
       pageName={DOWNLOADER_PAGE_NAME}
       sourceCodeLink={`${GITHUB_URL}/${DOWNLOADER_PAGE_NAME}`}
     />
