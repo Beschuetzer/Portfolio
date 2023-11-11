ee2f3b9430a13927338b5d57304cbe392c2d4619
Adam <62818816+Beschuetzer@users.noreply.github.com>
2023-11-10 21:34:43 -0600

    Finish BridgeDemo carousel

--- frontend/src/pages/examples/csharp/BridgeDemo.tsx
+++ frontend/src/pages/examples/csharp/BridgeDemo.tsx
@@ -1,160 +1,196 @@
 import React from "react";
-import { Carousel } from "../../../components/Carousel/Carousel";
+import { Carousel } from "react-thumbnail-carousel";
 
 import { CSharpLayout } from "./CSharpLayout";
 
 import video1 from "../../../clips/bridge-demo/1.mp4";
 import video2 from "../../../clips/bridge-demo/2.mp4";
 import video3 from "../../../clips/bridge-demo/3.mp4";
+import video1Thumbnail from "../../../clips/bridge-demo/1-thumbnail.png";
+import video2Thumbnail from "../../../clips/bridge-demo/2-thumbnail.png";
+import video3Thumbnail from "../../../clips/bridge-demo/3-thumbnail.png";
 
 import {
-	LIVE_BRIDGE_URL,
-	BRIDGE_DEMO_PAGE_NAME,
-	CAROUSEL_CLASSNAME,
-	C_SHARP_CLASSNAME,
+  LIVE_BRIDGE_URL,
+  BRIDGE_DEMO_PAGE_NAME,
+  C_SHARP_CLASSNAME,
 } from "../../../components/constants";
 import { EmbeddedLink } from "../../../components/EmbeddedLink";
 import { CSharpCardSection } from "./CSharpCardSection";
 import { CSharpSection } from "../../../types";
-import { functionToGetContainer, closeCarouselItem } from "../../../helpers";
 
 const sectionNames = [
-	"Requirements",
-	"Written Instructions",
-	"Video Instructions",
+  "Requirements",
+  "Written Instructions",
+  "Video Instructions",
 ];
 
 const sections: CSharpSection[] = [
-	{
-		name: sectionNames[0],
-		pageName: C_SHARP_CLASSNAME,
-		children: [
-			<React.Fragment>
-				<CSharpCardSection>
-					<p>
-						Since there is no AI currently implemented, runing a demo of A#Maj Bridge requires one to open two different browsers with two tabs in each browser (one of which has to be a "Private Browsing" tab or "Incognito" tab).  
-					</p>
-					<ol>
-						<li>Two separate browsers (neither can be IE)</li>
-						<li>
-							Browsers must have a way to create independent sessions (e.g.
-							"Private Window" in Firefox and "Incognito Mode" in Crome)
-						</li>
-					</ol>
-				</CSharpCardSection>
-			</React.Fragment>,
-		],
-	},
-	{
-		name: sectionNames[1],
-		pageName: C_SHARP_CLASSNAME,
-		children: [
-			<React.Fragment>
-				<CSharpCardSection>
-					<ol>
-						<li>Open both browsers</li>
-						<li>
-							Open one private browsing window for each browser (four windows
-							total)
-						</li>
-						<li>
-							Navigate to{" "}
-							<EmbeddedLink
-								isLocal={false}
-								href={`${LIVE_BRIDGE_URL}/login`}
-								addSpaces={false}>
-								here
-							</EmbeddedLink>
-							&nbsp;in each window
-						</li>
-						<li>
-							In each window login using one of the four usernames below and the
-							password (each window must login with a separate username
-							otherwise game-play will be impossible due to security reasons):
-							<ul className="padding-top-1 padding-bottom-1">
-								<li>Username 1: 'Test'</li>
-								<li>Username 2: 'TEST'</li>
-								<li>Username 3: 'tesT'</li>
-								<li>Username 4: 'test'</li>
-								<li>Password: 'test'</li>
-							</ul>
-						</li>
-						<li>
-							Explore the site and try creating a lobby to start a game to see
-							the actual gameplay
-						</li>
-						<li>See below section for videos explaining the above info</li>
-					</ol>
-				</CSharpCardSection>
-			</React.Fragment>,
-		],
-	},
-	{
-		hasCarousel: false,
-		name: sectionNames[2],
-		pageName: C_SHARP_CLASSNAME,
-		children: [
-			<section className={`${C_SHARP_CLASSNAME}__${CAROUSEL_CLASSNAME}`}>
-				<Carousel
-					items={[
-						{
-							itemSrc: video1,
-							description: "Setting Up",
-							videoOverlayText: "Four Windows At Once",
-							videoOverlayChildren: (
-								<div>
-									<CSharpCardSection>
-										This video show you how to open Chrome and Firefox and create private session windows
-									</CSharpCardSection>
-								</div>
-							),
-						},
-						{
-							itemSrc: video2,
-							description: "Logging In",
-							videoOverlayText: "Test is the Password ",
-							videoOverlayChildren: (
-								<div>
-									<CSharpCardSection>
-										Notice how each window uses a separate username but the password is the same.
-									</CSharpCardSection>
-								</div>
-							),
-						},
-						{
-							itemSrc: video3,
-							description: "Starting a Game",
-							videoOverlayText: "Starting",
-							videoOverlayChildren: (
-								<div>
-									<CSharpCardSection title="Create a Lobby">
-										After logging in, create a lobby by entering a name an clicking 'Go'.
-									</CSharpCardSection>
-									<CSharpCardSection title="Join the Lobby">
-									    Clicking 'Go' with a lobby name that is open will join that lobby.
-									</CSharpCardSection>
-								</div>
-							),
-						},
-					]}
-					numberOfItemsInCarouselWidthWise="3"
-					numberOfItemsToScrollOnClick="3"
-					functionToGetContainer={functionToGetContainer}
-					functionToRunOnClose={closeCarouselItem.bind(
-						null,
-						null as any,
-						`#${sectionNames[2].toLowerCase()}`,
-					)}
-				/>
-			</section>,
-		],
-	},
+  {
+    name: sectionNames[0],
+    pageName: C_SHARP_CLASSNAME,
+    children: [
+      <React.Fragment>
+        <CSharpCardSection>
+          <p>
+            Since there is no AI currently implemented, runing a demo of A#Maj
+            Bridge requires one to open two different browsers with two tabs in
+            each browser (one of which has to be a "Private Browsing" tab or
+            "Incognito" tab).
+          </p>
+          <ol>
+            <li>Two separate browsers (neither can be IE)</li>
+            <li>
+              Browsers must have a way to create independent sessions (e.g.
+              "Private Window" in Firefox and "Incognito Mode" in Crome)
+            </li>
+          </ol>
+        </CSharpCardSection>
+      </React.Fragment>,
+    ],
+  },
+  {
+    name: sectionNames[1],
+    pageName: C_SHARP_CLASSNAME,
+    children: [
+      <React.Fragment>
+        <CSharpCardSection>
+          <ol>
+            <li>Open both browsers</li>
+            <li>
+              Open one private browsing window for each browser (four windows
+              total)
+            </li>
+            <li>
+              Navigate to{" "}
+              <EmbeddedLink
+                isLocal={false}
+                href={`${LIVE_BRIDGE_URL}/login`}
+                addSpaces={false}
+              >
+                here
+              </EmbeddedLink>
+              &nbsp;in each window
+            </li>
+            <li>
+              In each window login using one of the four usernames below and the
+              password (each window must login with a separate username
+              otherwise game-play will be impossible due to security reasons):
+              <ul className="padding-top-1 padding-bottom-1">
+                <li>Username 1: 'Test'</li>
+                <li>Username 2: 'TEST'</li>
+                <li>Username 3: 'tesT'</li>
+                <li>Username 4: 'test'</li>
+                <li>Password: 'test'</li>
+              </ul>
+            </li>
+            <li>
+              Explore the site and try creating a lobby to start a game to see
+              the actual gameplay
+            </li>
+            <li>See below section for videos explaining the above info</li>
+          </ol>
+        </CSharpCardSection>
+      </React.Fragment>,
+    ],
+  },
+  {
+    hasCarousel: false,
+    name: sectionNames[2],
+    pageName: C_SHARP_CLASSNAME,
+    children: [
+      <Carousel
+        options={{
+          thumbnail: {
+            spacingStrategy: "max",
+          },
+          styling: {
+            container: {
+              padding: {
+                top: 14,
+                bottom: 14,
+                left: 28,
+                right: 28,
+              },
+            },
+          },
+        }}
+        items={[
+          {
+            srcMain: video1,
+            srcThumbnail: video1Thumbnail,
+            description: "Setting Up",
+            modal: {
+              sections: [
+                {
+                  title: "Four Windows at Once",
+                  text: "This video show you how to open Chrome and Firefox and create private session windows",
+                },
+              ],
+            },
+            video: {
+              sections: [
+                ["Opening First Browser", 6500],
+                ["Opening Icognito Mode in First Browser ", 4500],
+                ["Opening Second Browser", 4200],
+                ["Opening Icognito Mode in Second Browser ", 0],
+              ],
+            },
+          },
+          {
+            srcMain: video2,
+            srcThumbnail: video2Thumbnail,
+            description: "Logging In",
+            modal: {
+              sections: [
+                {
+                  title: "Test is the Password",
+                  text: " Notice how each window uses a separate username but the password is the same.",
+                },
+              ],
+            },
+            video: {
+              sections: [
+                ["Logging in as 'test'", ""],
+                ["Logging in as 'Test'", "8:00"],
+                ["Logging in as 'TEST'", "12:00"],
+                ["Logging in as 'tesT'", "15:00"],
+              ],
+            },
+          },
+          {
+            srcMain: video3,
+            srcThumbnail: video3Thumbnail,
+            description: "Starting a Game",
+            modal: {
+              sections: [
+                {
+                  title: "Create a Lobby",
+                  text: "After logging in, create a lobby by entering a name and clicking 'Go'.",
+                },
+                {
+                  title: "Join the Lobby",
+                  text: "Clicking 'Go' with a lobby name that is open will join that lobby.",
+                },
+              ],
+            },
+            video: {
+              sections: [
+                ["Creating a Lobby", ""],
+                ["Setting up the Lobby", "15:00"],
+                ["Starting the Game", "21:00"],
+                ["Bidding", "24:00"],
+              ],
+            },
+          },
+        ]}
+      />,
+    ],
+  },
 ];
 
 interface BridgeDemoProps {}
 
 export const BridgeDemo: React.FC<BridgeDemoProps> = () => {
-	return (
-		<CSharpLayout sections={sections} pageName={BRIDGE_DEMO_PAGE_NAME}/>
-	);
+  return <CSharpLayout sections={sections} pageName={BRIDGE_DEMO_PAGE_NAME} />;
 };
