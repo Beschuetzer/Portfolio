0f6dcf3af03464b5b0b5e464d3b1e4ee5b06b9b3
Adam <62818816+Beschuetzer@users.noreply.github.com>
2025-02-11 09:53:37 -0600

    fix: overflow styling issues causing sticky positioning to not work

--- frontend/src/components/navbar/sitenav/SiteNavContext.tsx
+++ frontend/src/components/navbar/sitenav/SiteNavContext.tsx
@@ -30,12 +30,12 @@ export const SiteNavProvider: React.FC<SiteNavProviderProps> = ({
 
   useEffect(() => {
     if (isOpen) {
-      document.body.style.overflow = "hidden";
+      document.body.style.overflowY = "hidden";
     } else {
-      document.body.style.overflow = "auto";
+      document.body.style.overflowY = "auto";
     }
     return () => {
-      document.body.style.overflow = "auto";
+      document.body.style.overflowY = "auto";
     };
   }, [isOpen]);
 
