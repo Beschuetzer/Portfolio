74a82d36e222f8a0f960813592fda9e8709707f8
Adam <62818816+Beschuetzer@users.noreply.github.com>
2025-02-14 06:01:18 -0600

    chore: add background-color style

--- frontend/src/components/navbar/sitenav/SiteNavBackground.tsx
+++ frontend/src/components/navbar/sitenav/SiteNavBackground.tsx
@@ -12,6 +12,7 @@ const SiteNavBackgroundStyled = styled.div<SiteNavStyledProps>`
   width: 200vw;
   z-index: -1;
   ${props => props.isopen ? "backdrop-filter: blur(.7rem) brightness(.5) drop-shadow(0 1.4rem 2.8rem var(--color-grey-80));" : ""}
+  background-color: rgba(0, 0, 0, .5);
 transition: filter .5s ease, -webkit-filter .5s ease;
 `;
 
