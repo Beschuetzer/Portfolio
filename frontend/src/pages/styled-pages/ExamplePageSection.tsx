c0114cd9a241f6b1337f039fc832630c930e27ab
Adam <62818816+Beschuetzer@users.noreply.github.com>
2025-02-16 07:12:50 -0600

    chore(ExamplePageSection): adjust styles

--- frontend/src/pages/styled-pages/ExamplePageSection.tsx
+++ frontend/src/pages/styled-pages/ExamplePageSection.tsx
@@ -9,10 +9,12 @@ import {
 import { INTER_SECTION_PADDING } from "./constants";
 import { useColorScheme } from "../../hooks/useColorScheme";
 import { StyledPageProps } from "./types";
+import { hexToRgba } from "../../components/navbar/sitenav/helpers";
 
 const Section = styled.section<LayoutStyledProps>`
   width: 100%;
   border-radius: ${getFontSizeCustom(2)};
+  border: 1px solid ${(props) => hexToRgba(props.colorscheme?.primary1, 0.33)};
   background: ${(props) => props.colorscheme?.primary4};
   display: flex;
   flex-direction: column;
@@ -22,7 +24,8 @@ const Section = styled.section<LayoutStyledProps>`
   overflow: hidden;
   max-width: ${SECTION_WIDTH_IN_PIXELS}px;
   z-index: 1;
-  box-shadow: rgba(43, 105, 251, 0.06) 0px 0px 1rem 1rem;
+  box-shadow: ${(props) => hexToRgba(props.colorscheme?.primary4, 0.06)} 0px 0px
+    1rem 1rem;
 
   ${respond.navSwitch`
     margin: 0 auto ${INTER_SECTION_PADDING} auto;  
