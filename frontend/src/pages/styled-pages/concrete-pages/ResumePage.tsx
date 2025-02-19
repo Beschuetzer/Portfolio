5690016ce9674f932d3f2d93831e6b408e834f32
Adam <62818816+Beschuetzer@users.noreply.github.com>
2025-02-19 08:33:16 -0600

    chore: add styling for ResumePageWorkHistory

--- frontend/src/pages/styled-pages/concrete-pages/ResumePage.tsx
+++ frontend/src/pages/styled-pages/concrete-pages/ResumePage.tsx
@@ -1,13 +1,16 @@
 import { ExamplePage, ExamplePageSectionProps } from "../ExamplePage";
 import {
+  BEST_BUY_URL,
   GITHUB_URL,
   PLAYLIST_SYNCER_PAGE_NAME,
+  YORK_B2E_URL,
 } from "../../../components/constants";
 import { ExamplePageParagraph } from "../ExamplePageParagraph";
 import { HoverEffect, LayoutStyledProps } from "../../../layouts/types";
 
 import { Quote } from "../../../components/Quote";
 import { ResumePageWorkHistory } from "./ResumePageWorkHistory";
+import { defaultFontSize } from "../../../styles/constants";
 
 export const RESUME_SECTION_TITLES = [
   "Overview",
@@ -26,19 +29,58 @@ const RESUME_SECTIONS: ExamplePageSectionProps[] = [
           text="You are not your résumé, you are your work."
         />
         <ExamplePageParagraph {...propsToAdd}>
-          Welcome! I am a developer with a passion for creating
-          applications that are both useful and enjoyable to use. Below you will
-          find my work history, a summary of my skills, and my references.
+          Welcome! I am a developer with a passion for creating applications
+          that are both useful and enjoyable to use. Below you will find my work
+          history, a summary of my skills, and my references.
         </ExamplePageParagraph>
         <ExamplePageParagraph>
-          Selecting a skill will open a modal with links to the Github repo and a live demo if available.
+          Selecting a skill will open a modal with links to the Github repo and
+          a live demo if available.
         </ExamplePageParagraph>
       </>
     ),
   },
   {
     name: RESUME_SECTION_TITLES[1],
-    renderContent: (propsToAdd: LayoutStyledProps) => <ResumePageWorkHistory />,
+    renderContent: (propsToAdd: LayoutStyledProps) => (
+      <ResumePageWorkHistory
+        containerProps={{
+          style: {
+            paddingTop: defaultFontSize
+          }
+        }}
+        items={[
+          {
+            employer: {
+              name: "Best Buy",
+              url: BEST_BUY_URL,
+            },
+            achievements: [
+              `Part of the Solution Sidekick (add link) team responsible for building a selling tool used by in-store employees.`,
+              `Played a decisive role in the development of the following features/enhancements: leads, accessory attachment, PDP, voice notes, caching, and displaying combo items in the basket.`,
+              `Discovered and fixed numerous bugs.`,
+            ],
+            dateEnd: "11/22",
+            dateStart: "02/22",
+            jobTitle: "Engineer Associate (Contract)",
+          },
+          {
+            employer: {
+              name: "York Solutions",
+              url: YORK_B2E_URL,
+            },
+            achievements:  [
+              `Learned Java (Spring Boot), Agile methodology, and improved React.js skills with the intent of starting a contract at Best Buy as a full`,
+              `Created a "Movie Night Recommendation" app leveraging Best Buy APIs in Spring Boot and React as
+              well as a team project.`,
+            ],
+            dateEnd: "02/22",
+            dateStart: "11/21",
+            jobTitle: "Barriers to Entry Java Full Stack Program",
+          },
+        ]}
+      />
+    ),
   },
 ];
 
--- frontend/src/pages/styled-pages/concrete-pages/ResumePageWorkHistory.tsx
+++ frontend/src/pages/styled-pages/concrete-pages/ResumePageWorkHistory.tsx
@@ -1,26 +1,122 @@
-import React from "react";
 import { styled } from "styled-components";
 import { LayoutStyledProps } from "../../../layouts/types";
 import { useColorScheme } from "../../../hooks/useColorScheme";
+import {
+  defaultFontSize,
+  fontSizeFive,
+  fontSizeSix,
+  fontSizeThree,
+  getFontSizeCustom,
+} from "../../../styles/constants";
+import { linkStyles } from "../../../styles/styles";
+import { HTMLAttributes } from "react";
+
+const Achievements = styled.ul<LayoutStyledProps>`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  padding: ${defaultFontSize} 0;
+`;
 
 const Container = styled.div<LayoutStyledProps>`
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
 `;
 
-type ResumePageWorkHistoryProps = {};
+const ItemContainer = styled.div<LayoutStyledProps>`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+`;
 
-export function ResumePageWorkHistory(
-  props: ResumePageWorkHistoryProps
-) {
-    const colorScheme = useColorScheme();
-    const propsToAdd: LayoutStyledProps = {
-        colorscheme: colorScheme,
-    };
+const EmployerName = styled.a<LayoutStyledProps>`
+  font-style: italic;
+  font-size: ${fontSizeThree};
+  ${linkStyles}
+`;
+
+const Header = styled.div<LayoutStyledProps>`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
+`;
+
+const HeaderTop = styled.div<LayoutStyledProps>`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+
+const TimeRange = styled.div<LayoutStyledProps>`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  font-size: ${fontSizeThree};
+  font-style: italic;
+`;
+
+const JobTitle = styled.h6<LayoutStyledProps>`
+  font-size: ${fontSizeFive};
+`;
+
+type ResumePageWorkHistoryItem = {
+  achievements: string[];
+  dateEnd: string;
+  dateStart: string;
+  employer: {
+    name: string;
+    url?: string;
+  };
+  jobTitle: string;
+};
+
+type ResumePageWorkHistoryProps = {
+  containerProps?: HTMLAttributes<HTMLDivElement>;
+  items: ResumePageWorkHistoryItem[];
+};
+
+export function ResumePageWorkHistory(props: ResumePageWorkHistoryProps) {
+  const { containerProps, items } = props;
+  const colorScheme = useColorScheme();
+  const propsToAdd: LayoutStyledProps = {
+    colorscheme: colorScheme,
+  };
   return (
-    <Container {...propsToAdd}>
-      <div>test</div>
+    <Container {...propsToAdd} {...containerProps}>
+      {items.map((item, index) => {
+        return (
+          <ItemContainer key={index}>
+            <Header>
+              <HeaderTop>
+                <JobTitle>{item.jobTitle}</JobTitle>
+                <TimeRange>
+                  <time dateTime={item.dateStart}>{item.dateStart}</time>
+                  <span>&nbsp;-&nbsp;</span>
+                  <time dateTime={item.dateEnd}>{item.dateEnd}</time>
+                </TimeRange>
+              </HeaderTop>
+              <EmployerName {...propsToAdd} href={item.employer.url}>
+                {item.employer.name}
+              </EmployerName>
+            </Header>
+
+            <Achievements {...propsToAdd}>
+              {item.achievements.map((achievement, index) => {
+                return <li key={index}>{achievement}</li>;
+              })}
+            </Achievements>
+          </ItemContainer>
+        );
+      })}
     </Container>
   );
 }
