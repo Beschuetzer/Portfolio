1e87138a9765b2f4f0078725c8919690e9e1c7e6
Adam <62818816+Beschuetzer@users.noreply.github.com>
2025-02-20 15:17:26 -0600

    fix: PageNav items being visible when site nat is open and is nav switch case

--- frontend/src/components/navbar/NavbarHeader.tsx
+++ frontend/src/components/navbar/NavbarHeader.tsx
@@ -5,6 +5,7 @@ import { SiteNavStyledProps } from "./sitenav/types";
 import { useColorScheme } from "../../hooks/useColorScheme";
 import { navbarHeaderNavSwitchHeightStyles } from "./sitenav/styles";
 import { hexToRgba } from "./sitenav/helpers";
+import { useSiteNav } from "./sitenav/SiteNavContext";
 
 type NavbarHeaderProps = {
   children?: React.ReactNode | React.ReactNode[];
@@ -14,7 +15,7 @@ const NavbarHeaderNav = styled.nav<SiteNavStyledProps>`
   position: fixed;
   top: 0;
   width: 100%;
-  z-index: 1000000;
+  z-index: ${props => props.isopen ? '10000000' : '1000000'};
 
   ${respond.navSwitch`
     background-color: ${(props: SiteNavStyledProps) =>
@@ -25,10 +26,12 @@ const NavbarHeaderNav = styled.nav<SiteNavStyledProps>`
 `;
 export function NavbarHeader(props: NavbarHeaderProps) {
   const { children } = props;
+  const { isOpen } = useSiteNav();
   const colorScheme = useColorScheme();
 
   const propsToAdd: SiteNavStyledProps = {
     colorscheme: colorScheme != null ? colorScheme : undefined,
+    isopen: isOpen, 
   };
   return <NavbarHeaderNav {...propsToAdd}>{children}</NavbarHeaderNav>;
 }
