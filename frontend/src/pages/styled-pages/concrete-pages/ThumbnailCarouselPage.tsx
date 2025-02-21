464b8648f92df3cf8f391b452f2c07b9e4379ceb
Adam <62818816+Beschuetzer@users.noreply.github.com>
2025-02-21 11:21:04 -0600

    chore: finish first use case section

--- frontend/src/pages/styled-pages/concrete-pages/ThumbnailCarouselPage.tsx
+++ frontend/src/pages/styled-pages/concrete-pages/ThumbnailCarouselPage.tsx
@@ -4,6 +4,16 @@ import { HoverEffect, LayoutStyledProps } from "../../../layouts/types";
 import { THUMBNAIL_CAROUSEL_PACKAGE_URL } from "../../../components/constants";
 import { ExamplePageLink } from "../ExamplePageLink";
 import { Carousel } from "react-thumbnail-carousel";
+import { getCarouselStylingOptions } from "../../../styles/styles";
+import { BREAK_POINTS } from "../../../styles/breakpoints";
+
+import defaultGif from "../../../clips/react-thumbnail-carousel/Default.gif";
+import fullscreenGif from "../../../clips/react-thumbnail-carousel/fullscreen.gif";
+import aspectRatioGif from "../../../clips/react-thumbnail-carousel/aspectRatio.gif";
+import descriptionModalGif from "../../../clips/react-thumbnail-carousel/descriptionModal.gif";
+import descriptionModalBuilderGif from "../../../clips/react-thumbnail-carousel/descriptionModalBuilder.gif";
+import { get } from "http";
+import { SECTION_WIDTH_IN_PIXELS } from "../../../styles/constants";
 
 export const THUMBNAIL_CAROUSEL_SECTION_NAMES = [
   "Overview",
@@ -42,12 +52,78 @@ const SECTIONS: ExamplePageSectionProps[] = [
       <>
         <ExamplePageParagraph>
           The first use case for the thumbnail carousel is to display a gallery
-          of images. This is useful for showcasing a collection of images in a
+          of images in a row. This is useful for showcasing a collection of images in a
           visually appealing way:
         </ExamplePageParagraph>
-        <Carousel items={[
-            {}
-        ]} />
+        <Carousel
+          options={{
+            styling: {
+              container: {
+                margin: {
+                  top: [
+                    [14],
+                    [12, parseInt(BREAK_POINTS.phone, 10), "max-width"],
+                  ],
+                },
+              },
+            },
+            thumbnail: {
+              ...getCarouselStylingOptions(propsToAdd.colorscheme).thumbnail,
+            },
+          }}
+          items={[
+            {
+              srcMain: defaultGif,
+              description: "Default layout navigation",
+            },
+            {
+              srcMain: fullscreenGif,
+              description: "Fullscreen Feature",
+            },
+          ]}
+        />
+        <ExamplePageParagraph>
+          If all of the items can't fit into the width of the parent, a
+          navigation bar will appear, allowing the user to navigate through the
+          items by pressing the left and right arrows or swiping:
+        </ExamplePageParagraph>
+        <Carousel
+          options={{
+            ...getCarouselStylingOptions(propsToAdd.colorscheme),
+            layout: {
+              itemDisplayLocation: "none",
+            },
+            styling: {
+              ...getCarouselStylingOptions(propsToAdd.colorscheme).styling,
+              container: {
+                margin: {
+                  top: [
+                    [14],
+                    [12, parseInt(BREAK_POINTS.phone, 10), "max-width"],
+                  ],
+                },
+              },
+            },
+            thumbnail: {
+              ...getCarouselStylingOptions(propsToAdd.colorscheme).thumbnail,
+              size: [[SECTION_WIDTH_IN_PIXELS / 2 - 50], [window.innerWidth / 2 - 50, parseInt(BREAK_POINTS.phone, 10), "max-width"]],
+            },
+          }}
+          items={[
+            {
+              srcMain: aspectRatioGif,
+              description: "Different aspect ratios",
+            },
+            {
+              srcMain: descriptionModalGif,
+              description: "Description modal",
+            },
+            {
+              srcMain: descriptionModalBuilderGif,
+              description: "Building a description modal",
+            },
+          ]}
+        />
       </>
     ),
   },
