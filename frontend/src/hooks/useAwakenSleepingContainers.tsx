c7b6bd839c7e581438c64a0119549429fb26c073
Adam <62818816+Beschuetzer@users.noreply.github.com>
2025-03-05 07:11:32 -0600

    chore update useAwakenSleepingContainers

--- frontend/src/hooks/useAwakenSleepingContainers.tsx
+++ frontend/src/hooks/useAwakenSleepingContainers.tsx
@@ -1,17 +1,27 @@
 import { useEffect } from "react";
 import { LIVE_BRIDGE_URL, LIVE_REPLAYS_URL } from "../components/constants";
 
+const NUMBER_OF_MINUTES = 10;
+const PING_INTERVAL = 60 * 1000 * NUMBER_OF_MINUTES;
+const GROCIFY_BFF_URL = "https://grocify-bff-ac27c2662495.herokuapp.com/";
+const URLS_TO_PING = [
+  GROCIFY_BFF_URL,
+  LIVE_BRIDGE_URL,
+  LIVE_REPLAYS_URL,
+];
+
 export const useAwakenSleepingContainers = () => {
-     useEffect(() => {
-        (async () => {
-          // Trigger a call to the URL behind the scenes
-          const bridgeUrlPromise = fetch(LIVE_BRIDGE_URL, {
+  useEffect(() => {
+    setInterval(() => {
+      (async () => {
+        const promises = [];
+        for (const url of URLS_TO_PING) {
+          promises.push(fetch(url, {
             mode: "no-cors",
-          });
-          const replaysUrlPromise = fetch(LIVE_REPLAYS_URL, {
-            mode: "no-cors",
-          });
-          await Promise.all([bridgeUrlPromise, replaysUrlPromise]);
-        })();
-      }, []);
-}
+          }));
+        }
+        await Promise.all(promises);
+      })();
+    }, PING_INTERVAL);
+  }, []);
+};
