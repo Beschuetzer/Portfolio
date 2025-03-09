a7cfc3b1052d0567d29616e426294879f1b96ecf
Adam <62818816+Beschuetzer@users.noreply.github.com>
2025-03-08 21:08:20 -0600

    chore: add fetch

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const routes = require('./routes/gitHubRoute');
 const path = require('path');
+const fetch = require('node-fetch');
 require('dotenv').config();
 
 app.use(express.json());
