cc0a4c29c282ddef1eafa8d34580e0ec8a1d1d37
Adam <62818816+Beschuetzer@users.noreply.github.com>
2025-03-19 05:55:41 -0500

    chore: add section effect

--- frontend/src/pages/styled-pages/ExamplePageSection.tsx
+++ frontend/src/pages/styled-pages/ExamplePageSection.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import { LayoutStyledProps } from "../../layouts/types";
 import { respond } from "../../styles/breakpoints";
@@ -12,7 +12,12 @@ import { StyledPageProps } from "./types";
 import { hexToRgba } from "../../components/navbar/sitenav/helpers";
 import { SITE_NAV_BUTTON_OPEN_BORDER_RADIUS_AMOUNT } from "../../components/navbar/sitenav/SiteNavButton";
 
-const Section = styled.section<LayoutStyledProps>`
+// Extend LayoutStyledProps with our custom prop for animation:
+interface SectionProps extends LayoutStyledProps {
+  isVisible: boolean;
+}
+
+const Section = styled.section<SectionProps>`
   font-size: ${defaultFontSize};
   width: 100%;
   border-radius: calc(${SITE_NAV_BUTTON_OPEN_BORDER_RADIUS_AMOUNT} / 2);
@@ -25,8 +30,12 @@ const Section = styled.section<LayoutStyledProps>`
   margin-bottom: ${INTER_SECTION_PADDING};
   max-width: ${SECTION_WIDTH_IN_PIXELS}px;
   z-index: 1;
-  box-shadow: ${(props) => hexToRgba(props.colorscheme?.primary4, 0.06)} 0px 0px
-    1rem 1rem;
+  box-shadow: ${(props) => hexToRgba(props.colorscheme?.primary4, 0.06)} 0px 0px 1rem 1rem;
+
+  // Animation styles:
+  transform: ${({ isVisible }) => isVisible ? "translate3d(0, 0, 0)" : "translate3d(-100%, 0, 0)"};
+  opacity: ${({ isVisible }) => isVisible ? 1 : 0};
+  transition: transform .5s ease-in, opacity .5s ease-in;
 
   ${respond.navSwitch`
     margin: 0 auto ${INTER_SECTION_PADDING} auto;  
@@ -43,8 +52,42 @@ export function ExamplePageSection(props: StyledPageProps) {
   const propsToAdd = {
     colorscheme: colorScheme,
   };
+
+  const sectionRef = useRef<HTMLElement>(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const sectionRefCopy = sectionRef.current;
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsVisible(true);
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      {
+        threshold: 0.1,
+      }
+    );
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current);
+    }
+    return () => {
+      if (sectionRefCopy) {
+        observer.unobserve(sectionRefCopy);
+      }
+    };
+  }, []);
+
   return (
-    <Section {...propsToAdd} {...htmlAttributes}>
+    <Section
+      ref={sectionRef}
+      isVisible={isVisible}
+      {...propsToAdd}
+      {...htmlAttributes}
+    >
       {children}
     </Section>
   );
