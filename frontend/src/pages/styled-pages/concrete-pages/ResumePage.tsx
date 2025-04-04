b07d063987e46d7cd8152781676acacd4d4adc1d
Adam <62818816+Beschuetzer@users.noreply.github.com>
2025-04-04 11:16:17 -0500

    chore: re-add reference

--- frontend/src/pages/styled-pages/concrete-pages/ResumePage.tsx
+++ frontend/src/pages/styled-pages/concrete-pages/ResumePage.tsx
@@ -219,13 +219,6 @@ const RESUME_SECTIONS: ExamplePageSectionProps[] = [
     renderContent: (propsToAdd: LayoutStyledProps) => (
       <ReferenceItems
         items={[
-          // {
-          //   name: "Troy McCabe",
-          //   relation: "Best Buy Manager",
-          //   phone: "913-209-2823",
-          //   email: "troymccabe@gmail.com",
-          //   linkedInUrl: "https://www.linkedin.com/in/troymccabe/",
-          // },
           {
             name: "Alissa Monroe",
             relation: "Best Buy Co-worker",
@@ -253,6 +246,13 @@ const RESUME_SECTIONS: ExamplePageSectionProps[] = [
             phone: "612-867-5333",
             email: "jeffwallen27@gmail.com",
             linkedInUrl: "https://www.linkedin.com/in/jeff-wallen/",
+          },
+          {
+            name: "Troy McCabe",
+            relation: "Best Buy Manager",
+            phone: "913-209-2823",
+            email: "troymccabe@gmail.com",
+            linkedInUrl: "https://www.linkedin.com/in/troymccabe/",
           },
           {
             name: "Scott Helland",
